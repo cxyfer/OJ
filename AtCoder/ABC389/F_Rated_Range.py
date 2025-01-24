071ef33bb9c51cd0a7bdd10cfffe9d59600629e2
cxyfer <gdst.tw@gmail.com>
2025-01-24 23:33:28 +0800

    ✨ feat: Update solutions for ABC389 problems
    - Refactor B_tcaF.py to utilize bisect for finding the position in factorial list.
    - Improve D_Squares_in_Circle.py comments for clarity on square center checks within a circle.
    - Enhance F_Rated_Range.py with clearer comments and maintainability in the LazySegTree implementation.

--- AtCoder/ABC389/F_Rated_Range.py
+++ AtCoder/ABC389/F_Rated_Range.py
@@ -7,16 +7,19 @@ queries = [int(input()) for _ in range(Q)]
 
 MX = max(max(r for _, r in contests), max(q for q in queries)) + 1
 lst = LazySegTree(op=max,
-                  e=0, 
+                  e=0,
                   mapping=lambda f, x: f+x,
                   composition=lambda f, g: f+g,
                   id_=0,
-                  v=list(range(MX)))
+                  v=list(range(MX))  # 初始值是當前 index 對應的 rating
+                  )
 
 for l, r in contests:
+    # 找到最小的 j 使得 f[j] >= l
     l = lst.max_right(0, lambda x: x < l)
+    # 找到最小的 j 使得 f[j] > r，其前一個位置即是最大的 j 使得 f[j] <= r
     r = lst.max_right(0, lambda x: x <= r)
-    lst.apply(l, r, 1)
+    lst.apply(l, r, 1)  # 將 [l, r) 的 rating 增加 1
 
 for q in queries:
     print(lst.get(q))
