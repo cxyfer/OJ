d25aad9144f971ee0ca010b2eea5d9660bf7ebae
cxyfer <gdst.tw@gmail.com>
2025-01-26 09:15:41 +0800

    ✨ feat: Enhance Combination Sum II solution with multiple implementation approaches
    - Add two different backtracking solutions for LeetCode 40
    - Implement variations in C++ and Python with detailed comments
    - Optimize duplicate handling and pruning techniques
    - Demonstrate different approaches to solving the combination sum problem

--- Leetcode/cpp/medium/40.cpp
+++ Leetcode/cpp/medium/40.cpp
@@ -4,34 +4,65 @@
  * [40] 组合总和 II
  */
 
-
 // @lcpr-template-start
 #include <bits/stdc++.h>
 using namespace std;
 // @lcpr-template-end
 // @lc code=start
-class Solution {
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
-        vector<int> path;
-        function<void(int, int)> dfs = [&](int i, int s) {
-            if (s == target) {
-                ans.push_back(path);
-                return;
-            }
-            for (int j = i; j < n; ++j) {
-                if (j > i && candidates[j] == candidates[j - 1]) continue;
-                if (s + candidates[j] > target) break;
-                path.push_back(candidates[j]);
-                dfs(j + 1, s + candidates[j]);
-                path.pop_back();
-            }
-        };
-        dfs(0, 0);
-        return ans;
-    }
+class Solution1 {
+ public:
+  vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    int n = candidates.size();
+    sort(candidates.begin(), candidates.end());
+    vector<vector<int>> ans;
+    vector<int> path;
+    auto dfs = [&](auto&& dfs, int i, int s) {
+      if (s == target) {
+        ans.push_back(path);
+        return;
+      }
+      if (i == n) return;
+      int x = candidates[i];
+      if (s + x > target) return;
+      path.push_back(x);
+      dfs(dfs, i + 1, s + x);
+      path.pop_back();
+      while (i < n && candidates[i] == x) {
+        i++;
+      }
+      dfs(dfs, i, s);
+    };
+    dfs(dfs, 0, 0);
+    return ans;
+  }
 };
+
+class Solution2 {
+ public:
+  vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    int n = candidates.size();
+    sort(candidates.begin(), candidates.end());
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    auto dfs = [&](auto&& dfs, int i, int s) {
+      if (s == target) {
+        ans.push_back(path);
+        return;
+      }
+      for (int j = i; j < n; ++j) {
+        if (j > i && candidates[j] == candidates[j - 1]) continue;
+        if (s + candidates[j] > target) break;
+        path.push_back(candidates[j]);
+        dfs(dfs, j + 1, s + candidates[j]);
+        path.pop_back();
+      }
+    };
+    dfs(dfs, 0, 0);
+    return ans;
+  }
+};
+
+// class Solution : public Solution1 {};
+class Solution : public Solution2 {};
 // @lc code=end
--- Leetcode/python3/medium/40_Combination Sum II.py
+++ Leetcode/python3/medium/40_Combination Sum II.py
@@ -10,33 +10,73 @@
 from preImport import *
 # @lcpr-template-end
 # @lc code=start
-class Solution:
+"""
+回溯 (Backtracking)
+1. 選或不選
+2. 枚舉選哪個
+"""
+class Solution1:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         n = len(candidates)
         candidates.sort()
         ans = []
         path = []
+
         def dfs(i: int, s: int):
             if s == target:
                 ans.append(path[:])
                 return
-            for j in range(i, n): # 考慮下一個選的元素
-                # 若當前元素和上一個元素相同，則不需要再考慮
-                # 沒選上一個，則也不能選第二個相同的元素
-                # 若有多個相同的元素，則只能從前面開始選
-                if j > i and candidates[j] == candidates[j - 1]: # 跳過重複的元素
+            if i == n:
+                return
+            
+            x = candidates[i]
+            if s + x > target:  # 剪枝
+                return
+            # 選
+            path.append(x)
+            dfs(i + 1, s + x)
+            path.pop()
+            # 不選
+            while i < n and candidates[i] == x:
+                i += 1
+            dfs(i, s)
+        dfs(0, 0)
+        return ans
+
+class Solution2:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        candidates.sort()
+        ans = []
+        path = []
+
+        def dfs(i: int, s: int):
+            if s == target:
+                ans.append(path[:])
+                return
+            for j in range(i, n):  # 考慮下一個選的元素
+                """
+                若當前元素和 [i, n) 範圍內的上一個元素相同，則不需要再考慮
+                這是因為沒選第一個，則也不能選第二個相同的元素，否則會重複計算
+                也就是說，若有多個相同的元素，則只能從第一個開始選
+                """
+                if j > i and candidates[j] == candidates[j - 1]:  # 跳過重複的元素
                     continue
-                if s + candidates[j] > target: # 剪枝
+                if s + candidates[j] > target:  # 剪枝
                     break
                 path.append(candidates[j])
                 dfs(j + 1, s + candidates[j])
                 path.pop()
         dfs(0, 0)
         return ans
+
+class Solution(Solution2):
+    pass
 # @lc code=end
 
+
 sol = Solution()
-print(sol.combinationSum2([2,5,2,1,2], 5))
+print(sol.combinationSum2([2, 5, 2, 1, 2], 5))  # [[1, 2, 2], [5]]
 
 
 #
@@ -49,4 +89,3 @@ print(sol.combinationSum2([2,5,2,1,2], 5))
 # @lcpr case=end
 
 #
-
