463ba8d40c0d3c7d3fa89bf3d4659d0c2793b7bb
cxyfer <gdst.tw@gmail.com>
2025-03-06 21:46:38 +0800

    ✨ feat: Add LeetCode 2965 Find Missing and Repeated Values Solution
    - Implement three solution approaches in C++ and Python
      1. Hash Table method
      2. Bit Manipulation technique
      3. Mathematical calculation
    - Efficiently find repeated and missing values in a grid
    - Demonstrate multiple problem-solving strategies
    - Provide O(n) time complexity solutions

--- Leetcode/cpp/easy/2965.cpp
+++ Leetcode/cpp/easy/2965.cpp
@@ -10,27 +10,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 // @lcpr-template-end
+/*
+    1. Hash Table
+    2. Bit Manipulation
+        - 260. Single Number III
+    3. Math
+*/
 // @lc code=start
 using LL = long long;
-class Solution {
+
+class Solution1 {
 public:
-    /*
-        1. Hash Table
-        2. Bit Manipulation
-            - 260. Single Number III
-        3. Math
-    */
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        // return solve1(grid);
-        // return solve2(grid);
-        return solve3(grid);
-    }
-    vector<int> solve1(vector<vector<int>>& grid) {
         int n = grid.size();
-        unordered_map<int, int> cnt;
-        for (int i=0; i<n; i++)
-            for (int j=0; j<n; j++)
-                cnt[grid[i][j]]++;
+        vector<int> cnt(n * n + 1);
+        for (auto& row : grid) for (auto& x : row) cnt[x]++;
         vector<int> ans(2);
         for (int i=1; i<=n*n; i++){
             if (cnt[i] == 2) ans[0] = i; // repeated, a
@@ -38,42 +32,48 @@ public:
         }
         return ans;
     }
-    vector<int> solve2(vector<vector<int>>& grid) {
+};
+
+class Solution2 {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
         // xor all numbers
         int s = 0; // xor of all numbers
-        for (int i=0; i<n; i++)
-            for (int j=0; j<n; j++)
-                s ^= grid[i][j];
-        for (int i=1; i<=n*n; i++)
-            s ^= i;
+        for (auto& row : grid) for (auto& x : row) s ^= x;
+        for (int i = 1; i <= n * n; i++) s ^= i;
         // group xor
         int lb = s & -s; // lowest bit of 1
         vector<int> ans(2);
-        for (int i=0; i<n; i++)
-            for (int j=0; j<n; j++)
-                ans[(grid[i][j] & lb) == 0] ^= grid[i][j];
-        for (int i=1; i<=n*n; i++)
-            ans[(i & lb) == 0] ^= i;
+        for (auto& row : grid) for (auto& x : row)
+            ans[(x & lb) == 0] ^= x;
+        for (int i = 1; i <= n * n; i++) ans[(i & lb) == 0] ^= i;
         // check if ans[0] is in grid
         for (auto& row : grid)
             if (find(row.begin(), row.end(), ans[0]) != row.end())
                 return ans;
         return {ans[1], ans[0]};
     }
-    vector<int> solve3(vector<vector<int>>& grid) {
+};
+
+class Solution3 {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
         LL m = n * n, d1 = 0, d2 = 0;
-        for (int i=0; i<n; i++)
-            for (int j=0; j<n; j++){
-                d1 += grid[i][j]; // sum of all numbers
-                d2 += grid[i][j] * grid[i][j]; // sum of all squares
-            }
+        for (auto& row : grid) for (auto& x : row){
+            d1 += x; // sum of all numbers
+            d2 += x * x; // sum of all squares
+        }
         d1 -= m * (m + 1) / 2; // a - b
         d2 -= m * (m + 1) * (m * 2 + 1) / 6; // a^2 - b^2 = (a + b)(a - b)
         return {(int) (d2 / d1 + d1) / 2, (int) (d2 / d1 - d1) / 2}; // (a, b)
     }
 };
+
+// class Solution : public Solution1 {};
+// class Solution : public Solution2 {};
+class Solution : public Solution3 {};
 // @lc code=end
 
 /*
--- Leetcode/python3/easy/2965_Find Missing and Repeated Values.py
+++ Leetcode/python3/easy/2965_Find Missing and Repeated Values.py
@@ -9,19 +9,15 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+    1. Hash Table
+    2. Bit Manipulation
+        - Similar to 260. Single Number III
+    3. Math
+"""
 # @lc code=start
-class Solution:
-    """
-        1. Hash Table
-        2. Bit Manipulation
-            - 260. Single Number III
-        3. Math
-    """
+class Solution1:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        # return self.solve1(grid)
-        return self.solve2(grid)
-        # return self.solve3(grid)
-    def solve1(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
         cnt = [0] * (n * n + 1)
         for row in grid:
@@ -34,17 +30,14 @@ class Solution:
             elif cnt[i] == 0: # missing, b
                 ans[1] = i
         return ans
-    def solve2(self, grid: List[List[int]]) -> List[int]:
+    
+class Solution2:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-        # 異或所有數字
-        xor = 0 # xor of all numbers
-        for row in grid:
-            for x in row:
-                xor ^= x
-        for x in range(1, n * n + 1):
-            xor ^= x
+        _xor = reduce(xor, (x for row in grid for x in row))
+        _xor ^= reduce(xor, (x for x in range(1, n * n + 1)))
         # 分組異或
-        lb = xor & -xor # 最低位的 1
+        lb = _xor & -_xor # 最低位的 1
         ans = [0, 0]
         for row in grid:
             for x in row:
@@ -52,13 +45,20 @@ class Solution:
         for x in range(1, n * n + 1):
             ans[x & lb == 0] ^= x # 分組異或
         return ans if ans[0] in [x for row in grid for x in row] else ans[::-1]
-    def solve3(self, grid: List[List[int]]) -> List[int]:
+    
+class Solution3:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n, m = len(grid), len(grid) ** 2
         s1 = sum(x for row in grid for x in row) # sum of all numbers
         s2 = sum(x * x for row in grid for x in row) # sum of all squares
         d1 = s1 - m * (m + 1) // 2 # a - b
         d2 = s2 - m * (m + 1) * (m * 2 + 1) // 6 # a^2 - b^2 = (a + b)(a - b)
         return [(d2 // d1 + d1) // 2, (d2 // d1 - d1) // 2] # (a, b)
+    
+# class Solution(Solution1):
+# class Solution(Solution2):
+class Solution(Solution3):
+    pass
 # @lc code=end
 
 sol = Solution()
