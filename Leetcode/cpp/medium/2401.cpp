faadfa67bf6de9f6035f134fd0c296179c3abd9a
cxyfer <gdst.tw@gmail.com>
2025-03-18 10:41:45 +0800

    ✨ feat: Add solutions for LeetCode problem 2401
    Implements three different solutions (two sliding window approaches and one optimized approach) for the "Longest Nice Subarray" problem in both C++ and Python. The solutions determine the longest subarray where the bitwise AND of any two elements is zero.

---
+++ Leetcode/cpp/medium/2401.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=2401 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2401] 最长优雅子数组
+ */
+
+
+// @lcpr-template-start
+#include <bits/stdc++.h>
+using namespace std;
+// @lcpr-template-end
+/*
+ *   在子陣列中，每個位元只能出現一次
+ *   1. 用 cnt 來記錄每個位元出現的次數、 over 來記錄有幾個位元出現超過一次
+ *   2. 用 or_val 來記錄當前每個位元是否出現過
+ *   3. LogTrick
+ */
+// @lc code=start
+class Solution1 {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0, left = 0, over = 0;
+        vector<int> cnt(32, 0);
+        for (int right = 0; right < n; right++) {
+            int x = nums[right];
+            // 1. 入窗口
+            for (int j = 0; j < 32; j++)
+                if (x & (1 << j))
+                    if (++cnt[j] == 2) over++;
+            // 2. 出窗口
+            while (over > 0) {
+                int y = nums[left++];
+                for (int j = 0; j < 32; j++)
+                    if (y & (1 << j))
+                        if (--cnt[j] == 1) over--;
+            }
+            // 3. 更新答案
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0, left = 0, over = 0, or_val = 0;
+        for (int right = 0; right < n; right++) {
+            int x = nums[right];
+            // 1. 要先出窗口才能入窗口
+            while (or_val & x) or_val ^= nums[left++];
+            // 2. 入窗口
+            or_val |= x;
+            // 3. 更新答案
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
+
+class Solution3 {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int right = 0; right < n; right++) {
+            int or_val = nums[right];
+            int left = right - 1;
+            while (left >= 0 && (or_val & nums[left]) == 0)
+                or_val |= nums[left--];
+            ans = max(ans, right - left);
+        }
+        return ans;
+    }
+};
+// class Solution : public Solution1 {};
+class Solution : public Solution2 {};
+// class Solution : public Solution3 {};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,3,8,48,10]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,1,5,11,13]\n
+// @lcpr case=end
+
+ */
+
---
+++ Leetcode/python3/medium/2401_Longest Nice Subarray.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=2401 lang=python3
+#
+# [2401] Longest Nice Subarray
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+"""
+    在子陣列中，每個位元只能出現一次
+    1. 用 cnt 來記錄每個位元出現的次數、 over 來記錄有幾個位元出現超過一次
+    2. 用 or_val 來記錄當前每個位元是否出現過
+    3. LogTrick
+"""
+# @lc code=start
+class Solution1:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        ans = left = over = 0
+        cnt = [0] * 32
+        for right, x in enumerate(nums):
+            # 1. 入窗口
+            for i in range(32):
+                if x & (1 << i):
+                    cnt[i] += 1
+                    if cnt[i] == 2:
+                        over += 1
+            # 2. 出窗口
+            while over > 0:
+                for i in range(32):
+                    if nums[left] & (1 << i):
+                        cnt[i] -= 1
+                        if cnt[i] == 1:
+                            over -= 1
+                left += 1
+            # 3. 更新答案
+            ans = max(ans, right - left + 1)
+        return ans
+    
+class Solution2:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        # 用 or_val 來記錄當前每個位元是否出現過
+        ans = left = or_val = 0
+        for right, x in enumerate(nums):
+            # 1. 要先出窗口才能入窗口
+            while or_val & x:
+                or_val ^= nums[left]
+                left += 1
+            # 2. 入窗口
+            or_val |= x
+            # 3. 更新答案
+            ans = max(ans, right - left + 1)
+        return ans
+
+class Solution3:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        ans = 0
+        for right, or_val in enumerate(nums):
+            left = right - 1
+            while left >= 0 and (or_val & nums[left]) == 0:
+                or_val |= nums[left]
+                left -= 1
+            ans = max(ans, right - left)
+        return ans
+
+# class Solution(Solution1):
+class Solution(Solution2):
+# class Solution(Solution3):
+    pass
+# @lc code=end
+
