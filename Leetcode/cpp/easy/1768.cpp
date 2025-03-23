9088f5d5e0f82fb148075c15fc79954f342ac48e
cxyfer <gdst.tw@gmail.com>
2025-03-23 19:27:59 +0800

    ✨ feat: Adds solution for 1768. Merge Strings Alternately
    Implements the `mergeAlternately` function in both C++ and Python.
    The function merges two input strings by taking characters in alternating order.
    It efficiently handles strings of different lengths.

---
+++ Leetcode/cpp/easy/1768.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=1768 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1768] Merge Strings Alternately
+ */
+
+
+// @lcpr-template-start
+#include <bits/stdc++.h>
+using namespace std;
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int ln = min(word1.size(), word2.size());
+        string ans;
+        for (int i = 0; i < ln; i++) {
+            ans += word1[i];
+            ans += word2[i];
+        }
+        ans += word1.substr(ln) + word2.substr(ln);
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abc"\n"pqr"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ab"\n"pqrs"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abcd"\n"pq"\n
+// @lcpr case=end
+
+ */
+
---
+++ Leetcode/python3/easy/1768_Merge Strings Alternately.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1768 lang=python3
+#
+# [1768] Merge Strings Alternately
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ln = min(len(word1), len(word2))
+        ans = ""
+        for i in range(ln):
+            ans += word1[i]
+            ans += word2[i]
+        ans += word1[ln:] + word2[ln:]
+        return ans
+# @lc code=end
+
