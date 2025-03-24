7831c6ec19b93586d5025c48f3a3058cff6b40ff
cxyfer <gdst.tw@gmail.com>
2025-03-24 22:33:43 +0800

    ⚡️ perf: Improve meeting day counting with radix sort
    Implements radix sort to optimize the sorting of meetings, replacing the standard sort. This enhances performance, particularly for large datasets.

--- Leetcode/cpp/medium/3169.cpp
+++ Leetcode/cpp/medium/3169.cpp
@@ -14,6 +14,29 @@ using namespace std;
  * 2. Prefix Sum + Line Sweep
  */
 // @lc code=start
+const int SIZE = 256;   // 2^8
+const int MASK = 255;   // 0xFF
+
+void radixSort(vector<pair<int, int>>& vec) {
+    vector<pair<int, int>> temp(vec.size());
+    vector<int> buckets(SIZE);
+    
+    for (int shift = 0; shift < 32; shift += 8) {
+        fill(buckets.begin(), buckets.end(), 0);
+        
+        for (const auto& x : vec)
+            buckets[(x.first >> shift) & MASK]++;
+        
+        for (int i = 1; i < SIZE; i++)
+            buckets[i] += buckets[i - 1];
+        
+        for (int i = vec.size() - 1; i >= 0; i--)
+            temp[--buckets[(vec[i].first >> shift) & MASK]] = vec[i];
+        
+        vec.swap(temp);
+    }
+}
+
 class Solution1a {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
@@ -39,12 +62,11 @@ public:
 class Solution1b {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
+        vector<pair<int, int>> vec;
+        for (auto& m : meetings) vec.emplace_back(m[0], m[1]);
+        radixSort(vec);
         int ans = days, st = 0, ed = -1;
-        for (auto& m : meetings) {
-            int x = m[0], y = m[1];
+        for (auto& [x, y] : vec) {
             if (x > ed) {
                 ans -= (ed - st + 1);  // [st, ed] is invalid
                 st = x;
@@ -96,8 +118,34 @@ public:
     }
 };
 
+class Solution2b {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        vector<pair<int, int>> vec;
+        for (auto& m : meetings) {
+            vec.emplace_back(m[0], 1);
+            vec.emplace_back(m[1] + 1, -1);
+        }
+        radixSort(vec);
+        
+        int ans = 0, s = 0, pre = 1;
+        for (int i = 0; i < vec.size(); i++) {
+            auto [d, v] = vec[i];
+            while (i + 1 < vec.size() && vec[i + 1].first == d) {
+                v += vec[++i].second;
+            }
+            if (s == 0) ans += d - pre;  // [pre, d - 1] is valid
+            s += v;
+            pre = d;
+        }
+        ans += days - pre + 1;  // [pre, days] is valid
+        return ans;
+    }
+};
+
 // using Solution = Solution1a;
-using Solution = Solution1b;
+// using Solution = Solution1b;
 // using Solution = Solution1c;
 // using Solution = Solution2;
+using Solution = Solution2b;
 // @lc code=end
