6cabd34f1a801da3a565e8a6060f300a483407af
cxyfer <gdst.tw@gmail.com>
2025-03-24 13:47:02 +0800

    ✨ feat: Implements multiple solutions for counting days
    Provides four different solutions (1a, 1b, 1c, and 2) to count valid days based on meeting intervals.
    
    Solution 1a, 1b and 1c use a merge-intervals approach.
    Solution 2 adopts a prefix sum and line sweep technique.
    Adds implementations in both C++ and Python.

--- Leetcode/cpp/medium/3169.cpp
+++ Leetcode/cpp/medium/3169.cpp
@@ -9,28 +9,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 // @lcpr-template-end
+/*
+ * 1. Merge Intervals
+ *    - Similar to 56. Merge Intervals
+ * 2. Prefix Sum + Line Sweep
+ */
 // @lc code=start
-class Solution {
+class Solution1a {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        vector<pair<int, int>> res;
-        for (int i = 0; i < meetings.size(); i++) {
-            int x = meetings[i][0], y = meetings[i][1];
-            if (res.empty() || res.back().second < x) {
-                res.push_back({x, y});
-            } else {
-                res.back().second = max(res.back().second, y);
-            }
+        sort(meetings.begin(), meetings.end());
+        
+        vector<pair<int, int>> merged;
+        for (auto& m : meetings) {
+            int x = m[0], y = m[1];
+            if (merged.empty() || merged.back().second < x) merged.emplace_back(x, y);
+            else merged.back().second = max(merged.back().second, y);
         }
+        
         int ans = days;
-        for (auto& p : res) {
-            ans -= p.second - p.first + 1;
-        }
+        for (auto& [x, y]: merged)  // [x, y] is invalid
+            ans -= y - x + 1;
         return ans;
     }
 };
+
+class Solution1b {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int ans = days;
+        int st = meetings[0][0], ed = meetings[0][1];
+        for (auto& m : meetings) {
+            int x = m[0], y = m[1];
+            if (x > ed) {
+                ans -= (ed - st + 1);  // [st, ed] is invalid
+                st = x;
+                ed = y;
+            }
+            else ed = max(ed, y);
+        }
+        ans -= (ed - st + 1);  // [st, ed] is invalid
+        return ans;
+    }
+};
+
+class Solution1c {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int ans = 0, ed = 0;
+        for (auto& m : meetings) {
+            int x = m[0], y = m[1];
+            if (x > ed) {
+                ans += x - ed - 1;  // [ed + 1, x - 1] is valid
+                ed = y;
+            }
+            else ed = max(ed, y);
+        }
+        ans += days - ed;  // [ed + 1, days] is valid
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        map<int, int> mp;
+        for (auto& m : meetings) {
+            mp[m[0]] += 1;
+            mp[m[1] + 1] -= 1;
+        }
+        int ans = 0, s = 0, pre = 1;
+        for (auto& [d, v] : mp) {
+            if (s == 0) ans += d - pre;  // [pre, d - 1] is valid
+            s += v;
+            pre = d;
+        }
+        ans += days - pre + 1;  // [pre, days] is valid
+        return ans;
+    }
+};
+
+using Solution = Solution1a;
+// using Solution = Solution1b;
+// using Solution = Solution1c;
+// using Solution = Solution2;
 // @lc code=end
 
--- Leetcode/python3/medium/3169_Count Days Without Meetings.py
+++ Leetcode/python3/medium/3169_Count Days Without Meetings.py
@@ -9,27 +9,79 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+1. Merge Intervals
+    - Similar to 56. Merge Intervals
+2. Prefix Sum + Line Sweep
+"""
 # @lc code=start
-class Solution:
-    """
-        Similar to 56. Merge Intervals
-    """
+class Solution1a:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         meetings.sort(key=lambda x: x[0])
-        res = [meetings[0]]
+        merged = [meetings[0]]
         for x, y in meetings:
-            if x > res[-1][1]:
-                res.append([x, y])
+            if x > merged[-1][1]:
+                merged.append([x, y])
             else:
-                res[-1][1] = max(res[-1][1], y)
+                merged[-1][1] = max(merged[-1][1], y)
         ans = days
-        for x, y in res:
-            ans -= y - x + 1
+        for x, y in merged:
+            ans -= y - x + 1  # [x, y] is invalid
         return ans
+
+class Solution1b:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key=lambda x: x[0])
+        ans = days
+        st, ed = meetings[0]
+        for x, y in meetings:
+            if x > ed:
+                ans -= (ed - st + 1)  # [st, ed] is invalid
+                st, ed = x, y
+            else:
+                ed = max(ed, y)
+        ans -= (ed - st + 1)  # [st, ed] is invalid
+        return ans
+class Solution1c:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key=lambda x: x[0])
+        ans = ed = 0
+        for x, y in meetings:
+            if x > ed:
+                ans += x - ed - 1  # [ed + 1, x - 1] is valid
+                ed = y
+            else:
+                ed = max(ed, y)
+        ans += days - ed  # [ed + 1, days] is valid
+        return ans
+
+class Solution2:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        mp = defaultdict(int)
+        for x, y in meetings:
+            mp[x] += 1
+            mp[y + 1] -= 1
+        ans = s = 0
+        pre = 1
+        for d in sorted(mp.keys()):
+            if s == 0:
+                ans += d - pre  # [pre, d - 1] is valid
+            s += mp[d]
+            pre = d
+        ans += days - pre + 1  # [pre, days] is valid
+        return ans
+    
+# Solution = Solution1a
+# Solution = Solution1b
+# Solution = Solution1c
+Solution = Solution2
 # @lc code=end
 
-
-
+sol = Solution()
+print(sol.countDays(10, [[5,7],[1,3],[9,10]]))  # 2
+print(sol.countDays(5, [[2,4],[1,3]]))  # 1
+print(sol.countDays(6, [[1,6]]))  # 0
+print(sol.countDays(8, [[3,4],[4,8],[2,5],[3,8]]))  # 1
 #
 # @lcpr case=start
 # 10\n[[5,7],[1,3],[9,10]]\n
