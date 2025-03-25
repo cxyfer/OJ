cd61d2dad897887de4f55c6d3bbbc0fbf512301f
cxyfer <gdst.tw@gmail.com>
2025-03-25 08:47:03 +0800

    🐛 fix: update grid cut validation logic and add tests

--- Leetcode/python3/medium/3394_Check if Grid can be Cut into Sections.py
+++ Leetcode/python3/medium/3394_Check if Grid can be Cut into Sections.py
@@ -10,37 +10,34 @@
 from preImport import *
 # @lcpr-template-end
 # @lc code=start
-class Solution:
+class Solution1:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        xPlus, yPlus = defaultdict(int), defaultdict(int)
-        xMinus, yMinus = defaultdict(int), defaultdict(int)
+        X, Y = defaultdict(int), defaultdict(int)
         for x1, y1, x2, y2 in rectangles:
-            xPlus[x1] += 1
-            xMinus[x2] += 1
-            yPlus[y1] += 1
-            yMinus[y2] += 1
+            X[x1 + 0.1] += 1
+            X[x2 - 0.1] -= 1
+            Y[y1 + 0.1] += 1
+            Y[y2 - 0.1] -= 1
         
-        def check(plus, minus):
+        def check(mp):
             res = cur = 0
-            keys = set(plus.keys()) | set(minus.keys())
-            for k in sorted(keys):
-                cur -= minus[k]
+            for k, v in sorted(mp.items()):
+                cur += v
                 if cur == 0:
                     res += 1
-                cur += plus[k]
-            return res
-        
-        res1 = check(xPlus, xMinus)
-        res2 = check(yPlus, yMinus)
-        return res1 >= 4 or res2 >= 4
+            return res >= 3
+
+        return check(X) or check(Y)
+
+Solution = Solution1
 # @lc code=end
 
 sol = Solution()
-print(sol.checkValidCuts(5, [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]))
-
-print(sol.checkValidCuts(4, [[0,0,1,1],[2,0,3,4],[0,2,2,3],[3,0,4,3]]))
-
-print(sol.checkValidCuts(4, [[0,2,2,4],[1,0,3,2],[2,2,3,4],[3,0,4,2],[3,2,4,4]]))
+print(sol.checkValidCuts(5, [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]))  # True
+print(sol.checkValidCuts(4, [[0,0,1,1],[2,0,3,4],[0,2,2,3],[3,0,4,3]]))  # True
+print(sol.checkValidCuts(4, [[0,2,2,4],[1,0,3,2],[2,2,3,4],[3,0,4,2],[3,2,4,4]]))  # False
+# Add missing test case
+print(sol.checkValidCuts(6, [[4,0,5,2],[0,5,4,6],[4,5,6,6]]))  # False
 
 #
 # @lcpr case=start
