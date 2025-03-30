3b48a2f0d03cff62a855696de0242fb27ef7756d
cxyfer <gdst.tw@gmail.com>
2025-03-30 20:40:36 +0800

    ♻️ refactor: Use C++23 lambda recursion & cleanup

--- Leetcode/cpp/easy/543.cpp
+++ Leetcode/cpp/easy/543.cpp
@@ -4,30 +4,32 @@
  * [543] 二叉树的直径
  */
 
+
+// @lcpr-template-start
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lcpr-template-end
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int ans = 0;
-        auto dfs = [&](auto&& dfs, TreeNode* root) -> int {
+        auto dfs = [&](this auto&& dfs, TreeNode* root) -> int {
             if (root == nullptr) return 0;
-            int left = dfs(dfs, root->left);
-            int right = dfs(dfs, root->right);
+            int left = dfs(root->left);
+            int right = dfs(root->right);
             ans = max(ans, left + right);
             return max(left, right) + 1;
         };
-        dfs(dfs, root);
+        dfs(root);
         return ans;
     }
 };
--- Leetcode/python3/easy/543_Diameter of Binary Tree.py
+++ Leetcode/python3/easy/543_Diameter of Binary Tree.py
@@ -10,12 +10,6 @@
 from preImport import *
 # @lcpr-template-end
 # @lc code=start
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         ans = 0
