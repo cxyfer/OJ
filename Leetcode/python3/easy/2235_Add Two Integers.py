7f4fc00b1fa573602841d83acbe002b0d318bfc9
cxyfer <gdst.tw@gmail.com>
2025-03-30 20:44:29 +0800

    ✨ feat: Add solution for LeetCode 2235

---
+++ Leetcode/python3/easy/2235_Add Two Integers.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=2235 lang=python3
+#
+# [2235] Add Two Integers
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+# @lc code=start
+class Solution: sum = add
+# @lc code=end
+
