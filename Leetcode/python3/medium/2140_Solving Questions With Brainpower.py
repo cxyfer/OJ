b5f409d6938e96d61de3a9335a4ce1af900c2182
cxyfer <gdst.tw@gmail.com>
2025-04-01 12:33:38 +0800

    ♻️ refactor: simplify function and update solution class

--- Leetcode/python3/medium/2140_Solving Questions With Brainpower.py
+++ Leetcode/python3/medium/2140_Solving Questions With Brainpower.py
@@ -13,12 +13,13 @@ from preImport import *
 class Solution1:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
+        # f(i) 表示從第 i 個問題開始最多可以獲得的分數
         @cache
-        def f(i: int) -> int: # 從第 i 個問題開始考慮最多可以獲得多少分數
+        def f(i: int) -> int:  
             if i >= n:
                 return 0
-            res1 = f(i + 1) # 不選第 i 個問題
-            res2 = questions[i][0] + f(i + questions[i][1] + 1) # 選第 i 個問題
+            res1 = f(i + 1)  # 不選第 i 個問題
+            res2 = questions[i][0] + f(i + questions[i][1] + 1)  # 選第 i 個問題
             return max(res1, res2)
         return f(0)
     
@@ -30,9 +31,8 @@ class Solution2:
             f[i] = max(f[i + 1], questions[i][0] + f[min(i + questions[i][1] + 1, n)])
         return f[0]
     
-# class Solution(Solution1):
-class Solution(Solution2):
-    pass
+# Solution = Solution1
+Solution = Solution2
 # @lc code=end
 
 sol = Solution()
