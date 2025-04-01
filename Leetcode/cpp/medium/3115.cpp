999f34e46f0ab7112bdb258c108a2afcdab8f78c
cxyfer <gdst.tw@gmail.com>
2025-04-01 23:15:10 +0800

    ♻️ refactor: simplify prime number initialization loop

--- Leetcode/cpp/medium/3115.cpp
+++ Leetcode/cpp/medium/3115.cpp
@@ -15,13 +15,10 @@ const int N = 105;
 vector<bool> is_prime(N, true);
 auto init = []() {
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i < N; i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j < N; j += i) {
+    for (int i = 2; i * i < N; i++)
+        if (is_prime[i])
+            for (int j = i * i; j < N; j += i)
                 is_prime[j] = false;
-            }
-        }
-    }
     return 0;
 }();
 
