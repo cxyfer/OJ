07851dc28eda7a0559f117289f33e49f85cb4b6d
cxyfer <gdst.tw@gmail.com>
2025-04-02 16:14:43 +0800

    ✨ feat: add optimized solutions for LeetCode 2873 & 2874
    Implements two solutions to find the maximum triplet value in an array.
    
    The first solution uses prefix and suffix arrays to find the maximum product.
    
    The second solution uses a greedy approach to maintain the maximum difference and maximum value seen so far.
    
    This commit refactors the code to include both solutions and allows easy switching between them.

--- Leetcode/cpp/medium/2874.cpp
+++ Leetcode/cpp/medium/2874.cpp
@@ -2,7 +2,7 @@
  * @lc app=leetcode.cn id=2874 lang=cpp
  * @lcpr version=30204
  *
- * [2874] 有序三元组中的最大值 II
+ * [2874] Maximum Value of an Ordered Triplet I
  */
 
 
@@ -10,8 +10,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 // @lcpr-template-end
+/*
+ * Greedy
+ * 為了找到 (nums[i] - nums[j]) * nums[k] 的最大值
+ * 基於貪心思路，可以使 (nums[i] - nums[j]) 和 nums[k] 越大越好
+ * 由於需要滿足 i < j < k，所以又有兩種思路：
+ * 1. 枚舉 nums[j]，然後找最大的 nums[i] 和 nums[k]，可以透過前後綴分解實現
+ * 2. 枚舉 nums[k]，維護前綴中的最大差值 (nums[i] - nums[j])，並維護前綴中最大的 nums[i] 以更新最大差值
+ */
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
@@ -27,8 +35,24 @@ public:
         return ans;
     }
 };
-// @lc code=end
 
+class Solution2 {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int mx = LLONG_MIN / 2, mx_diff = LLONG_MIN / 2;
+        long long ans = 0;
+        for (int x : nums) {
+            ans = max(ans, (long long)mx_diff * x);
+            mx_diff = max(mx_diff, mx - x);  // (nums[i] - nums[j])
+            mx = max(mx, x);  // nums[i]
+        }
+        return ans;
+    }
+};
+
+// using Solution = Solution1;
+using Solution = Solution2;
+// @lc code=end
 
 
 /*
--- Leetcode/python3/medium/2874_Maximum Value of an Ordered Triplet II.py
+++ Leetcode/python3/medium/2874_Maximum Value of an Ordered Triplet II.py
@@ -8,19 +8,41 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+Greedy
+為了找到 (nums[i] - nums[j]) * nums[k] 的最大值
+基於貪心思路，可以使 (nums[i] - nums[j]) 和 nums[k] 越大越好
+由於需要滿足 i < j < k，所以又有兩種思路
+
+1. 枚舉 nums[j]，然後找最大的 nums[i] 和 nums[k]，可以透過前後綴分解實現
+2. 枚舉 nums[k]，維護前綴中的最大差值 (nums[i] - nums[j])，並維護前綴中最大的 nums[i] 以更新最大差值
+"""
 # @lc code=start
-class Solution:
+class Solution1:
     def maximumTripletValue(self, nums: List[int]) -> int:
         n = len(nums)
         suf = [0] * n
-        suf[-1] = nums[-1]
+        suf[-1] = nums[-1]  # 後綴最大值
         for i in range(n - 2, -1, -1):
             suf[i] = max(suf[i + 1], nums[i])
         ans = 0
-        pre_mx = nums[0]
+        pre_mx = nums[0]  # 前綴最大值
         for i in range(1, n - 1):
             ans = max(ans, (pre_mx - nums[i]) * suf[i + 1])
             pre_mx = max(pre_mx, nums[i])
         return ans
+
+class Solution2:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        ans = 0
+        mx = mx_diff = float('-inf')
+        for x in nums:
+            ans = max(ans, mx_diff * x)
+            mx_diff = max(mx_diff, mx - x)  # (nums[i] - nums[j])
+            mx = max(mx, x)  # nums[i]
+        return ans
+
+# Solution = Solution1
+Solution = Solution2
 # @lc code=end
 
