028207da4c02b7fcc76f1a244c26b26c5b28326e
cxyfer <gdst.tw@gmail.com>
2025-04-08 10:35:55 +0800

    ✨ feat: Add solution for Codeforces 1791F
    Implements a solution using Union-Find to optimize range updates.
    Updates involve replacing numbers with the sum of their digits.
    The Union-Find structure skips elements already reduced to single digits.

---
+++ Codeforces/灵茶羊蹄/2025/2025-04/1791F.py
@@ -0,0 +1,48 @@
+class UnionFind:
+    __slots__ = ['n', 'pa']
+
+    def __init__(self, n: int):
+        self.n = n
+        self.pa = list(range(n)) # 父節點
+
+    def find(self, x: int) -> int:
+        while self.pa[x] != x:
+            self.pa[x] = self.pa[self.pa[x]]
+            x = self.pa[x]
+        return x
+
+    def union(self, x: int, y: int) -> bool:
+        fx, fy = self.find(x), self.find(y)
+        if fx == fy:
+            return False
+        if fx > fy:
+            fx, fy = fy, fx
+        self.pa[fx] = fy  # 向右合併
+        return True
+
+t = int(input())
+
+def calc(x):
+    res = 0
+    while x:
+        res += x % 10
+        x //= 10
+    return res
+
+for _ in range(t):
+    n, q = map(int, input().split())
+    A = list(map(int, input().split()))
+    uf = UnionFind(n + 1)
+    for _ in range(q):
+        op, *args = map(int, input().split())
+        if op == 1:
+            l, r = map(lambda x : x - 1, args)
+            idx = uf.find(l)
+            while (idx <= r):
+                A[idx] = calc(A[idx])
+                if A[idx] < 10:
+                    uf.union(idx, idx + 1)
+                idx = uf.find(idx + 1)
+        else:
+            idx = args[0] - 1
+            print(A[idx])
