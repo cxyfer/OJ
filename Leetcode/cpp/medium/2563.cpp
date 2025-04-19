ca7fe3dd238246fe5f920f1f62a961b2dcfb651e
cxyfer <gdst.tw@gmail.com>
2025-04-19 20:18:16 +0800

    ⚡️ perf: Implement two pointers solution for fair pairs
    Implements a two-pointer approach to count fair pairs, improving performance compared to the binary search method. This approach utilizes the prefix sum or mathematical inclusion-exclusion principle to efficiently count pairs within the specified range.

--- Leetcode/cpp/medium/2563.cpp
+++ Leetcode/cpp/medium/2563.cpp
@@ -10,18 +10,23 @@
 using namespace std;
 // @lcpr-template-end
 /*
-    Binary Search
-    由於順序不影響 Fair Pairs 的對數，只需要確保不重複計算即可，因此可以先對nums進行排序。
+1. Sorting + Binary Search
+由於順序不影響 Fair Pairs 的對數，只需要確保不重複計算即可，因此可以先對nums進行排序。
 
-    然後對於每個 i，找到有多少 j 可以使 lower <= nums[i] + nums[j] <= upper
-    即 lower - nums[i] <= nums[j] <= upper - nums[i]，這可以透過二分搜尋來找到。
+然後對於每個 i，找到有多少 j 可以使 lower <= nums[i] + nums[j] <= upper
+即 lower - nums[i] <= nums[j] <= upper - nums[i]，這可以透過二分搜尋來找到。
 
-    但注意不能包含 (i, i) 這種情況，以及不能重複計算。
-    這可以透過限制二分的範圍在 [0, i) 或 [i + 1, n) 來解決：
-    或是也能先不管，但先扣掉 (i, i) 的情況後，最後再除以 2 即可。
+但注意不能包含 (i, i) 這種情況，以及不能重複計算。
+這可以透過限制二分的範圍在 [0, i) 或 [i + 1, n) 來解決：
+或是也能先不管，但先扣掉 (i, i) 的情況後，最後再除以 2 即可。
+
+2. Sorting + Two Pointers
+使用前綴和或數學排容的思想，求 lo <= x + y <= hi 的對數，等同求 x + y <= hi 的對數減去 x + y < lo 的對數。
+而如果我們能確保 x 從小枚舉到大，那麼滿足條件的 y 只會越來越小，
+如此一來我們就可以使用 Two Pointers 來找到滿足條件的 y 了。
 */
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         int n = nums.size();
@@ -35,5 +40,24 @@ public:
         return ans;
     }
 };
-// @lc code=end
+class Solution2 {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        auto f = [&](int hi) -> long long {
+            long long res = 0;
+            int j = n - 1;
+            for (int i = 0; i < n && j > i; i++) {
+                while (j > i && nums[i] + nums[j] > hi) j--;
+                res += j - i;
+            }
+            return res;
+        };
+        return f(upper) - f(lower - 1);
+    }
+};
 
+// using Solution = Solution1;
+using Solution = Solution2;
+// @lc code=end
--- Leetcode/python3/medium/2563_Count the Number of Fair Pairs.py
+++ Leetcode/python3/medium/2563_Count the Number of Fair Pairs.py
@@ -10,18 +10,23 @@
 from preImport import *
 # @lcpr-template-end
 """
-    Binary Search
-    由於順序不影響 Fair Pairs 的對數，只需要確保不重複計算即可，因此可以先對nums進行排序。
+1. Sorting + Binary Search
+由於順序不影響 Fair Pairs 的對數，只需要確保不重複計算即可，因此可以先對nums進行排序。
 
-    然後對於每個 i，找到有多少 j 可以使 lower <= nums[i] + nums[j] <= upper
-    即 lower - nums[i] <= nums[j] <= upper - nums[i]，這可以透過二分搜尋來找到。
+然後對於每個 i，找到有多少 j 可以使 lower <= nums[i] + nums[j] <= upper
+即 lower - nums[i] <= nums[j] <= upper - nums[i]，這可以透過二分搜尋來找到。
 
-    但注意不能包含 (i, i) 這種情況，以及不能重複計算。
-    這可以透過限制二分的範圍在 [0, i) 或 [i + 1, n) 來解決：
-    或是也能先不管，但先扣掉 (i, i) 的情況後，最後再除以 2 即可。
+但注意不能包含 (i, i) 這種情況，以及不能重複計算。
+這可以透過限制二分的範圍在 [0, i) 或 [i + 1, n) 來解決：
+或是也能先不管，但先扣掉 (i, i) 的情況後，最後再除以 2 即可。
+
+2. Sorting + Two Pointers
+使用前綴和或數學排容的思想，求 lo <= x + y <= hi 的對數，等同求 x + y <= hi 的對數減去 x + y < lo 的對數。
+而如果我們能確保 x 從小枚舉到大，那麼滿足條件的 y 只會越來越小，
+如此一來我們就可以使用 Two Pointers 來找到滿足條件的 y 了。
 """
 # @lc code=start
-class Solution:
+class Solution1:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         ans = 0
         nums.sort()
@@ -30,6 +35,26 @@ class Solution:
             r = bisect_right(nums, upper - x, 0, i) - 1 # [0, idx)
             ans += r - l + 1
         return ans
+    
+class Solution2:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        n = len(nums)
+        nums.sort()
+        # 計算有多少個 pair 滿足 nums[i] + nums[j] <= hi
+        def f(hi: int) -> int:
+            res = 0
+            j = n - 1
+            for i, x in enumerate(nums):
+                while j > i and x + nums[j] > hi:  # 太大了，需要縮小右端點
+                    j -= 1
+                if j == i:
+                    break
+                res += j - i
+            return res
+        return f(upper) - f(lower - 1)
+
+# Solution = Solution1
+Solution = Solution2
 # @lc code=end
 
 
