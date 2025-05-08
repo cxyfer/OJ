7be0e275b82ba881fa868ba936984d4396dbd523
cxyfer <gdst.tw@gmail.com>
2025-05-08 19:38:57 +0800

    ✨ feat: Add C++ solution, refine Python for #3342

--- Leetcode/python3/medium/3342_Find Minimum Time to Reach Last Room II.py
+++ Leetcode/python3/medium/3342_Find Minimum Time to Reach Last Room II.py
@@ -13,33 +13,25 @@ from preImport import *
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
         n, m = len(moveTime), len(moveTime[0])
-        DIR = [(-1,0), (1,0), (0,-1), (0,1)]
-        
-        # dist = [[[float('inf')] * 2 for _ in range(m)] for _ in range(n)]
-        # dist[0][0][0] = 0
-        # hp = [(0, 0, 0, 0)] # (time, x, y, z)
-        dist = [[float('inf')] * m for _ in range(n)]
+        DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        dist = [[float('inf') for _ in range(m)] for _ in range(n)]
         dist[0][0] = 0
-        hp = [(0, 0, 0)] # (time, x, y)      
+
+        hp = [(0, 0, 0)]  # (time, x, y)
         while hp:
-            # t, x, y, z = heappop(hp)
             t, x, y = heappop(hp)
             if x == n - 1 and y == m - 1:
                 return t
-            # if t > dist[x][y][z]: continue
-            if t > dist[x][y]: continue
+            if t > dist[x][y]:
+                continue
             for dx, dy in DIR:
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < n and 0 <= ny < m:
-                    # nt = max(t, moveTime[nx][ny]) + (1 if z == 0 else 2)
-                    # nz = 1 - z
-                    # if nt < dist[nx][ny][nz]:
-                    #     dist[nx][ny][nz] = nt
-                    #     heappush(hp, (nt, nx, ny, nz))
-                    nt = max(t, moveTime[nx][ny]) + (2 if (x + y) & 1 else 1)
-                    if nt < dist[nx][ny]:
-                        dist[nx][ny] = nt
-                        heappush(hp, (nt, nx, ny))
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                nt = max(t, moveTime[nx][ny]) + (2 if (x + y) & 1 else 1)
+                if nt < dist[nx][ny]:
+                    dist[nx][ny] = nt
+                    heappush(hp, (nt, nx, ny))
         return -1
 # @lc code=end
 
