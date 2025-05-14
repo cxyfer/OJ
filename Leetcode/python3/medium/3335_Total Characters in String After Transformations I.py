a807724ae9e5e48f2e9ce6659c396aae0d984d83
cxyfer <gdst.tw@gmail.com>
2025-05-14 12:16:34 +0800

    ⚡️ perf: Optimize matrix multiplication and exponentiation
    Improves performance of matrix multiplication and exponentiation using list comprehension and iterative squaring. This reduces the time complexity for large inputs.

--- Leetcode/python3/medium/3335_Total Characters in String After Transformations I.py
+++ Leetcode/python3/medium/3335_Total Characters in String After Transformations I.py
@@ -34,14 +34,8 @@ class Matrix:
 
     def __mul__(self, other):
         assert len(self.mat[0]) == len(other.mat)
-        p, q, r = len(self.mat), len(other.mat), len(other.mat[0])
-        res = [[0] * r for _ in range(p)]
-        for k in range(q):
-            for i in range(p):
-                for j in range(r):
-                    res[i][j] += self.mat[i][k] * other.mat[k][j]
-                    res[i][j] %= MOD
-        return Matrix(res)
+        return Matrix([[sum(x * y for x, y in zip(row, col)) % MOD for col in zip(*other.mat)]
+                for row in self.mat])
     
     def __pow__(self, k):
         assert len(self.mat) == len(self.mat[0])
@@ -58,10 +52,11 @@ class Matrix:
     def mul_pow(self, k, x):
         assert len(self.mat[0]) == len(x.mat)
         res = x
+        base = self
         while k > 0:
             if k & 1:
-                res = self * res
-            self = self * self
+                res = base * res
+            base *= base
             k >>= 1
         return res
     
@@ -91,7 +86,33 @@ class Solution2:
             ans %= MOD
         return ans
     
-Solution = Solution1
+nums = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+M = Matrix([[0] * 26 for _ in range(26)])
+for i in range(26):
+    for k in range(1, nums[i] + 1):
+        j = (i + k) % 26
+        M[i][j] = 1
+pow_M = [None] * 32
+pow_M[0] = M
+for i in range(1, 32):
+    pow_M[i] = pow_M[i - 1] * pow_M[i - 1]
+
+class Solution3:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        cnt = [0] * 26
+        for ch in s:
+            cnt[ord(ch) - ord('a')] += 1
+        f = Matrix([[1] for _ in range(26)])
+        k = 0
+        while t > 0:
+            if t & 1:
+                f = pow_M[k] * f
+            t >>= 1
+            k += 1
+        return sum(f[i][0] * cnt[i] for i in range(26)) % MOD
+
+# Solution = Solution1
 # Solution = Solution2
+Solution = Solution3
 # @lc code=end
 
