919a9370954493fc3e370a33a5bd395f4f20ed81
cxyfer <gdst.tw@gmail.com>
2025-05-14 12:21:38 +0800

    ⚡️ perf: Optimize matrix multiplication and exponentiation
    Improves matrix multiplication and exponentiation
    performance by using list comprehension and numpy.

--- Template/Math/Matrix.py
+++ Template/Math/Matrix.py
@@ -5,14 +5,8 @@ class Matrix:
 
     def __mul__(self, other):
         assert len(self.mat[0]) == len(other.mat)
-        p, q, r = len(self.mat), len(other.mat), len(other.mat[0])
-        res = [[0] * r for _ in range(p)]
-        for k in range(q):
-            for i in range(p):
-                for j in range(r):
-                    res[i][j] += self.mat[i][k] * other.mat[k][j]
-                    res[i][j] %= MOD
-        return Matrix(res)
+        return Matrix([[sum(x * y for x, y in zip(row, col)) % MOD for col in zip(*other.mat)]
+                for row in self.mat])
     
     def __pow__(self, k):
         assert len(self.mat) == len(self.mat[0])
@@ -29,10 +23,11 @@ class Matrix:
     def mul_pow(self, k, x):
         assert len(self.mat[0]) == len(x.mat)
         res = x
+        base = self
         while k > 0:
             if k & 1:
-                res = self * res
-            self = self * self
+                res = base * res
+            base *= base
             k >>= 1
         return res
     
@@ -41,9 +36,39 @@ class Matrix:
 
     def __str__(self):
         return "\n".join(map(lambda x: " ".join(map(str, x)), self.mat))
+    
+"""
+    numpy version
+"""
+
+import numpy as np
+
+def matrix_power(A: np.ndarray, n: int, mod: int):
+    res = np.eye(len(A), dtype=object)
+    while n > 0:
+        if n & 1:
+            res = A @ res % mod
+        A = A @ A % mod
+        n >>= 1
+    return res
+
+def matrix_power_mul(A: np.ndarray, n: int, x: np.ndarray, mod: int):
+    assert len(A[0]) == len(x)
+    res = x
+    while n > 0:
+        if n & 1:
+            res = A @ res % mod
+        A = A @ A % mod
+        n >>= 1
+    return res
 
 if __name__ == "__main__":
     mat = Matrix([[1, 1], [1, 0]])
     x = Matrix([[1], [0]])
     print(mat ** 10 * x)
+
+    mat = np.array([[1, 1], [1, 0]])
+    x = np.array([[1], [0]])
+    print(matrix_power(mat, 10, MOD) @ x % MOD)
+    print(matrix_power_mul(mat, 10, x, MOD))
 
