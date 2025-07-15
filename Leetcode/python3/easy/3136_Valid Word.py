548069a76ec31360cb1e51f7b14932d21e5941c6
cxyfer <gdst.tw@gmail.com>
2025-07-15 14:54:33 +0800

    ♻️ refactor: Simplify word validation expression

--- Leetcode/python3/easy/3136_Valid Word.py
+++ Leetcode/python3/easy/3136_Valid Word.py
@@ -7,5 +7,5 @@ from preImport import *
 # @lc code=start
 class Solution:
     def isValid(self, word: str) -> bool:
-        return (lambda w : (len(w) >= 3 and w.isalnum() and any(c in "aeiou" for c in w) and any(c.isalpha() and c.lower() not in "aeiou" for c in w)))(word.lower())
+        return len(w := word.lower()) >= 3 and w.isalnum() and any(c in "aeiou" for c in w) and any(c.isalpha() and c not in "aeiou" for c in w)
 # @lc code=end
