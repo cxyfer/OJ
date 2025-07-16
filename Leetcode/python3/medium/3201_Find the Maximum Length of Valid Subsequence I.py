2b35f5430dc84d40e522afe5bf4c181474753bd8
cxyfer <gdst.tw@gmail.com>
2025-07-16 15:17:57 +0800

    ⚡️ perf: Optimize alternating subsequence calculation
    Eliminates redundant checks for alternating subsequences. The longest alternating subsequence can always start with `nums[0]`, allowing calculation to focus solely on the sequence matching `nums[0]`'s parity.

--- Leetcode/python3/medium/3201_Find the Maximum Length of Valid Subsequence I.py
+++ Leetcode/python3/medium/3201_Find the Maximum Length of Valid Subsequence I.py
@@ -8,6 +8,14 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+考慮全奇、全偶、奇偶交錯(奇先、偶先)，共 4 種情況。
+
+如果是 **奇偶交錯** 的話，則 nums[0] 一定可以出現在最長的子序列中，可以用矛盾法證明：
+令 seq 為最長的子序列且不包含 nums[0]，則，
+- 若 nums[0] 的奇偶性與 seq[0] 相同，則可以用 nums[0] 替換 seq[0]，子序列長度相同；
+- 若 nums[0] 的奇偶性與 seq[0] 不同，則可以產生更長的子序列，產生矛盾
+"""
 # @lc code=start
 class Solution1:
     def maximumLength(self, nums: List[int]) -> int:
@@ -21,7 +29,8 @@ class Solution1:
                     res += 1
                     b ^= 1
             return res
-        return max(f(0), f(1), cnt[0], cnt[1])
+        # return max(f(0), f(1), cnt[0], cnt[1])
+        return max(f(nums[0] & 1), cnt[0], cnt[1])
 
 class Solution2:
     def maximumLength(self, nums: List[int]) -> int:
