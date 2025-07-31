ca196be5f6a832cf17350a8fc58327f2fe915c00
cxyfer <gdst.tw@gmail.com>
2025-07-31 19:36:57 +0800

    ✨ feat: Implement diverse solutions for problem 1521
    Adds multiple algorithmic approaches for solving the "Find a Value of a Mysterious Function Closest to Target" problem. This includes optimized iterative methods, two-pointer techniques, and a segment tree approach. Provides a broader range of solutions to analyze performance characteristics.

--- Leetcode/cpp/hard/1521.cpp
+++ Leetcode/cpp/hard/1521.cpp
@@ -4,37 +4,215 @@
  * [1521] 找到最接近目标值的函数值
  */
 
-
 // @lcpr-template-start
 #include <bits/stdc++.h>
 using namespace std;
 // @lcpr-template-end
 // @lc code=start
-class Solution {
+class Solution1a {
 public:
-    /*
-        Same to 3171. Find Subarray With Bitwise AND Closest to K
-        AND 只會讓數字變小，所以可以用 Stack 來保存所有可能的 AND 結果
-    */
-    int closestToTarget(vector<int>& arr, int target) {
-        int n = arr.size();
+    int closestToTarget(vector<int>& nums, int k) {
+        int ans = INT_MAX;
+        unordered_set<int> st, st2;
+        for (int x : nums) {
+            st2.clear();
+            for (int y : st) st2.insert(y & x);
+            st2.insert(x);
+            swap(st, st2);
+            for (int y : st) ans = min(ans, abs(y - k));
+        }
+        return ans;
+    }
+};
+
+class Solution1b {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
         int ans = INT_MAX;
         vector<int> st;
-        for (int x : arr) {
-            vector<int> st2;
-            st2.push_back(x);
-            for (int y : st) {
-                if ((y & x) != st2.back()) {
-                    st2.push_back(y & x);
-                }
-            }
+        for (int x : nums) {
+            vector<int> st2 = {x};
+            for (int y : st)
+                if ((y & x) != st2.back()) st2.push_back(y & x);
             st = st2;
-            for (int y : st) {
-                ans = min(ans, abs(y - target));
+            for (int y : st) ans = min(ans, abs(y - k));
+        }
+        return ans;
+    }
+};
+
+class Solution1c {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, abs(nums[i] - k));
+            for (int j = i - 1; j >= 0; j--) {
+                if ((nums[j] & nums[i]) == nums[j]) break;
+                nums[j] &= nums[i];
+                ans = min(ans, abs(nums[j] - k));
             }
         }
         return ans;
     }
 };
-// @lc code=end
 
+class Solution2 {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        int left = 0, cur = 0;
+        vector<int> cnt(32, 0);
+        for (int right = 0; right < n; right++) {
+            for (int b = 0; b < 32; b++) {
+                if (nums[right] & (1 << b)) {
+                    if (++cnt[b] == (right - left + 1)) cur |= 1 << b;
+                } else {
+                    cur &= ~(1 << b);
+                }
+            }
+            ans = min(ans, abs(cur - k));
+            while (left < right && cur <= k) {
+                for (int b = 0; b < 32; b++) {
+                    if (nums[left] & (1 << b)) {
+                        cnt[b]--;
+                    } else {
+                        if (cnt[b] == (right - (left + 1) + 1)) cur |= 1 << b;
+                    }
+                }
+                left++;
+                ans = min(ans, abs(cur - k));
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution3a {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        // Stack 由上到下保存 AND(nums[i...bottom]), AND(nums[i+1...bottom])
+        // 之結果
+        vector<int> st = {-1};
+        int left = 0, right_and = -1;
+        for (int right = 0; right < n; right++) {
+            right_and &= nums[right];
+            while (left <= right && (st.back() & right_and) <= k) {
+                ans = min(ans, k - (st.back() & right_and));
+                st.pop_back();
+                left += 1;
+                // 重新構建 Stack
+                if (st.empty()) {
+                    st.push_back(-1);
+                    for (int i = right; i >= left; i--)
+                        st.push_back(st.back() & nums[i]);
+                    right_and = -1;
+                }
+            }
+            if (left <= right) ans = min(ans, (st.back() & right_and) - k);
+        }
+        return ans;
+    }
+};
+
+class Solution3b {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        // 原地使用 nums 作為 Stack，由上到下保存 AND(nums[i...bottom]),
+        // AND(nums[i+1...bottom]) 之結果
+        int left = 0, bottom = 0, right_and = -1;
+        for (int right = 0; right < n; right++) {
+            right_and &= nums[right];
+            while (left <= right && (nums[left] & right_and) <= k) {
+                ans = min(ans, k - (nums[left] & right_and));
+                left += 1;
+                // 重新構建 Stack
+                if (bottom < left) {
+                    for (int i = right - 1; i >= left; i--)
+                        nums[i] &= nums[i + 1];
+                    bottom = right;
+                    right_and = -1;
+                }
+            }
+            if (left <= right) ans = min(ans, (nums[left] & right_and) - k);
+        }
+        return ans;
+    }
+};
+
+class SegmentTree {
+private:
+    vector<int> nums;
+    vector<int> tree;
+
+public:
+    SegmentTree(vector<int>& nums) {
+        int n = nums.size();
+        this->nums = vector<int>(n + 1, 0);
+        for (int i = 1; i <= n; i++) this->nums[i] = nums[i - 1];  // 1-indexed
+        this->tree = vector<int>(4 * n, -1);
+        build(1, 1, n);
+    }
+
+    void build(int o, int left, int right) {
+        if (left == right) {
+            tree[o] = nums[left];
+            return;
+        }
+        int mid = (left + right) / 2;
+        build(2 * o, left, mid);
+        build(2 * o + 1, mid + 1, right);
+        tree[o] = merge(2 * o, 2 * o + 1);
+    }
+
+    int merge(int left_child, int right_child) {
+        return tree[left_child] & tree[right_child];
+    }
+
+    int query(int o, int left, int right, int l, int r) {
+        if (l <= left && right <= r) return tree[o];
+        int mid = (left + right) / 2;
+        int ans = -1;
+        if (l <= mid) ans &= query(2 * o, left, mid, l, r);
+        if (r > mid) ans &= query(2 * o + 1, mid + 1, right, l, r);
+        return ans;
+    }
+};
+
+class Solution4 {
+public:
+    int closestToTarget(vector<int>& nums, int k) {
+        int n = nums.size();
+        SegmentTree seg(nums);
+        int ans = INT_MAX;
+        for (int i = 1; i <= n; i++) {
+            int left = i, right = n;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (seg.query(1, 1, n, i, mid) <= k)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            if (left <= n) ans = min(ans, k - seg.query(1, 1, n, i, left));
+            if (right >= i) ans = min(ans, seg.query(1, 1, n, i, right) - k);
+        }
+        return ans;
+    }
+};
+
+// using Solution = Solution1a;
+// using Solution = Solution1b;
+// using Solution = Solution1c;
+// using Solution = Solution2;
+using Solution = Solution3a;
+// using Solution = Solution3b;
+// using Solution = Solution4;
+// @lc code=end
--- Leetcode/python3/hard/1521_Find a Value of a Mysterious Function Closest to Target.py
+++ Leetcode/python3/hard/1521_Find a Value of a Mysterious Function Closest to Target.py
@@ -9,28 +9,133 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+Similar to 3171. Find Subarray With Bitwise OR Closest to K
+把 OR 換成 AND 即是本題
+"""
 # @lc code=start
-class Solution:
-    """
-        Same to 3171. Find Subarray With Bitwise AND Closest to K
-        AND 只會讓數字變小，所以可以用 Stack 來保存所有可能的 AND 結果
-    """
-    def closestToTarget(self, arr: List[int], target: int) -> int:
-        n = len(arr)
-        ans = float("inf")
+class Solution1a:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
+        st = set()
+        ans = float('inf')
+        for x in nums: # 枚舉右端點
+            # 保存以 x 為右端點的所有 AND 結果
+            st = {y & x for y in st}
+            st.add(x)
+            ans = min(ans, min(abs(y - k) for y in st))
+        return ans
+
+class Solution1b:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
         st = []
-        for x in arr:
+        ans = float('inf')
+        for x in nums: # 枚舉右端點
+            # 保存以 x 為右端點的所有 AND 結果，注意由於 AND 的性質，這裡的 st2 是遞減的
             st2 = [x]
             for y in st:
+                # 保持有序性質以便去重，省去一個 set
                 if y & x != st2[-1]:
                     st2.append(y & x)
+            # 更新答案
+            for y in st2:
+                ans = min(ans, abs(y - k))
             st = st2
-            for y in st:
-                ans = min(ans, abs(y - target))
         return ans
+
+class Solution1c:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        # 枚舉右端點 i，在枚舉 i 時將 nums[j] 原地修改成 AND(nums[j...i])
+        # 此時有 A_j ⊇ A_{j+1} ⊇ A_{j+2} ⊇ ... ⊇ A_i 的性質，即 A_{j+1} 是 A_j 的子集，以此類推
+        for i, x in enumerate(nums):
+            ans = min(ans, abs(x - k))
+            for j in range(i - 1, -1, -1):
+                # 如果 x 是 nums[j] 的子集，則 x 也會是 nums[j-1] 的子集，以此類推，故不用更新
+                if nums[j] & x == nums[j]:
+                    break
+                nums[j] &= x
+                ans = min(ans, abs(nums[j] - k))
+        return ans
+
+class Solution2:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        left = cur = 0
+        cnt = [0] * 32
+        for right, x in enumerate(nums):
+            for b in range(32):
+                if x & (1 << b):
+                    cnt[b] += 1
+                    if cnt[b] == (right - left + 1):
+                        cur |= 1 << b
+                else:
+                    cur &= ~(1 << b)
+            ans = min(ans, abs(cur - k))
+            while left < right and cur <= k:
+                for b in range(32):
+                    if nums[left] & (1 << b):
+                        cnt[b] -= 1
+                    else:
+                        if cnt[b] == (right - (left + 1) + 1):
+                            cur |= 1 << b
+                left += 1
+                ans = min(ans, abs(cur - k))
+        return ans
+    
+class Solution3a:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        # Stack 由上到下保存 AND(nums[i...bottom]), AND(nums[i+1...bottom]) 等結果
+        st = [-1]  # 加入哨兵，避免 st 為空
+        left, right_and = 0, -1
+        for right, x in enumerate(nums):
+            right_and &= x
+            while left <= right and st[-1] & right_and <= k:
+                ans = min(ans, k - (st.pop() & right_and))
+                left += 1
+                # 重新構建 Stack
+                if not st:
+                    st.append(-1)  # 哨兵
+                    for i in range(right, left - 1, -1):
+                        st.append(st[-1] & nums[i])
+                    right_and = -1
+            if left <= right:
+                ans = min(ans, (st[-1] & right_and) - k)
+        return ans
+    
+class Solution3b:
+    def closestToTarget(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        # 原地使用 nums 作為 Stack，由上到下保存 AND(nums[i...bottom]), AND(nums[i+1...bottom]) 之結果
+        left = bottom = 0
+        right_and = -1
+        for right, x in enumerate(nums):
+            right_and &= x
+            while left <= right and nums[left] & right_and <= k:
+                ans = min(ans, k - (nums[left] & right_and))
+                left += 1
+                # 重新構建 Stack
+                if bottom < left:
+                    for i in range(right - 1, left - 1, -1):
+                        nums[i] &= nums[i + 1]
+                    bottom = right
+                    right_and = -1
+            if left <= right:
+                ans = min(ans, (nums[left] & right_and) - k)
+        return ans
+
+# Solution = Solution1a
+# Solution = Solution1b
+# Solution = Solution1c
+# Solution = Solution2
+Solution = Solution3a
+# Solution = Solution3b
 # @lc code=end
 
-
+sol = Solution()
+print(sol.closestToTarget([9,12,3,7,15], 5))  # 2
+print(sol.closestToTarget([1000000,1000000,1000000], 1))  # 999999
+print(sol.closestToTarget([1,2,4,8,16], 0))  # 0
 
 #
 # @lcpr case=start
