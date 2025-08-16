f98f0f7b536d09473d3c16305e45ab7626d22bf4
cxyfer <gdst.tw@gmail.com>
2025-08-16 14:29:56 +0800

    ✨ feat: Enhance Lazy Segment Tree for genericity
    Introduce `SegInfo` to encapsulate node data, enabling flexible aggregation operations.
    Implement templating in C++ and add a `build` method for initial array construction.
    Adapt the structure to support various range queries, improving reusability and modularity for different problem types like sum queries.

--- Template/RMQ/Lazy1.cpp
+++ Template/RMQ/Lazy1.cpp
@@ -2,92 +2,124 @@
 using namespace std;
 #define endl '\n'
 /*
-    Lazy Segment Tree
-    - 動態開點，只有當需要用到子節點時，才會創建子節點
+Lazy Segment Tree
+- 動態開點，只有當需要用到子節點時，才會創建子節點
+- 使用 SegInfo 來維護要維護的資訊，好處是直接可以使用 + 運算符來合併兩個 SegInfo，缺點是會有重新創建 SegInfo 物件的效能損失
+- 基本上只需要 SegInfo 的合併方法，以及 apply 方法即可，若遇到效能問題，可以考慮把需要重新創建 SegInfo 物件的部分改為直接賦值
 
-    Problem:
-    - 731. My Calendar II
-    - 732. My Calendar III
+Problem:
+- 731. My Calendar II
+- 732. My Calendar III
+- P3372 【模板】线段树 1
 */
 
+template <typename T>
+struct SegInfo {
+    T s;
+    SegInfo() : s(0) {}
+    SegInfo(T s) : s(s) {}
+
+    SegInfo operator+(SegInfo const& other) const {
+        return SegInfo(s + other.s);
+    }
+
+    SegInfo operator+=(SegInfo const& other) {
+        s += other.s;
+        return *this;
+    }
+};
+
+template <typename T>
 struct SegNode {
-    SegNode *ls, *rs; // left child, right child
-    int val, lazy; // value, lazy tag
+    SegNode *ls, *rs;  // left child, right child
+    SegInfo<T> val;    // value
+    T lazy;            // lazy tag
     SegNode() : ls(nullptr), rs(nullptr), val(0), lazy(0) {}
 };
 
+template <typename T>
 class SegmentTree {
 public:
-    SegNode *root;
-    
-    SegmentTree() {
-        root = new SegNode();
+    SegmentTree(vector<T>& nums) {
+        n = nums.size() - 1;
+        root = new SegNode<T>();
+        build(root, 1, n, nums);
+    }
+
+    void update(int l, int r, T v) {
+        _update(root, 1, n, l, r, v);
+    }
+
+    SegInfo<T> query(int l, int r) {
+        return _query(root, 1, n, l, r);
+    }
+
+private:
+    int n;
+    SegNode<T>* root;
+    void build(SegNode<T>* node, int left, int right, vector<T>& nums) {
+        if (left == right) {
+            node->val = SegInfo<T>(nums[left]);
+            return;
+        }
+        int mid = left + ((right - left) >> 1);
+        node->ls = new SegNode<T>();
+        node->rs = new SegNode<T>();
+        build(node->ls, left, mid, nums);
+        build(node->rs, mid + 1, right, nums);
+        pushup(node);  // Push up node value
     }
 
     // Update the range [l, r] with value v
-    void update(SegNode *node, int left, int right, int l, int r, int v) {
+    void _update(SegNode<T>* node, int left, int right, int l, int r, T v) {
         if (l <= left && right <= r) {
-            node->lazy += v;
-            node->val += v;
+            // Update node value (Customized)
+            apply(node, left, right, v);
             return;
         }
-        pushdown(node); // Push down lazy tags
+        pushdown(node, left, right);
         int mid = left + ((right - left) >> 1);
-        if (l <= mid) update(node->ls, left, mid, l, r, v);
-        if (r > mid) update(node->rs, mid + 1, right, l, r, v);
-        pushup(node); // Push up node value
+        if (l <= mid) _update(node->ls, left, mid, l, r, v);
+        if (r > mid) _update(node->rs, mid + 1, right, l, r, v);
+        pushup(node);  // Push up node value
+    }
+
+    // Update node value (Customized)
+    void apply(SegNode<T>* node, int left, int right, T v) {
+        node->val.s += v * (right - left + 1);
+        node->lazy += v;
     }
 
     // Query the range [l, r]
-    int query(SegNode *node, int left, int right, int l, int r) {
+    SegInfo<T> _query(SegNode<T>* node, int left, int right, int l, int r) {
         if (l <= left && right <= r) {
             return node->val;
         }
-        // Ensure all lazy tags have been pushed down
-        pushdown(node);
+        pushdown(node, left, right);
         int mid = left + ((right - left) >> 1);
-        // Calculate answer
-        int ans = 0;
-        if (l <= mid) ans = query(node->ls, left, mid, l, r);
-        if (r > mid) ans = max(ans, query(node->rs, mid + 1, right, l, r));
+        // Calculate answer (Customized)
+        SegInfo<T> ans = SegInfo<T>();
+        if (l <= mid) ans = ans + _query(node->ls, left, mid, l, r);
+        if (r > mid) ans = ans + _query(node->rs, mid + 1, right, l, r);
         return ans;
     }
 
     // Push down lazy tags
-    void pushdown(SegNode *node) {
-        if (node->ls == nullptr) node->ls = new SegNode();
-        if (node->rs == nullptr) node->rs = new SegNode();
+    void pushdown(SegNode<T>* node, int left, int right) {
+        if (node->ls == nullptr) node->ls = new SegNode<T>();
+        if (node->rs == nullptr) node->rs = new SegNode<T>();
         if (node->lazy != 0) {
-            node->ls->lazy += node->lazy;
-            node->rs->lazy += node->lazy;
-            node->ls->val += node->lazy;
-            node->rs->val += node->lazy;
-            node->lazy = 0; // Clear current node's lazy tag
+            int mid = left + ((right - left) >> 1);
+            // Update node value (Customized)
+            apply(node->ls, left, mid, node->lazy);
+            apply(node->rs, mid + 1, right, node->lazy);
+            node->lazy = 0;
         }
     }
 
     // Push up node value
-    void pushup(SegNode *node) {
-        // Update method
+    void pushup(SegNode<T>* node) {
+        // Update method (Customized)
         node->val = node->ls->val + node->rs->val;
     }
 };
-
-
-
-int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    int n, q; cin >> n >> q;
-    int op, l, r, v;
-    SegmentTree seg;
-    while (q--) {
-        cin >> op;
-        if (op == 1) {
-            cin >> l >> r >> v;
-            seg.update(seg.root, 1, n, l, r, v);
-        } else {
-            cin >> l >> r;
-            cout << seg.query(seg.root, 1, n, l, r) << endl;
-    }
-    return 0;
-}
--- Template/RMQ/Lazy1.py
+++ Template/RMQ/Lazy1.py
@@ -1,69 +1,112 @@
-from typing import *
+from typing import List
 
 """
-    Lazy Segment Tree
-    - 動態開點，只有當需要用到子節點時，才會創建子節點
+Lazy Segment Tree
+- 動態開點，只有當需要用到子節點時，才會創建子節點
+- 使用 SegInfo 來維護要維護的資訊，好處是直接可以使用 + 運算符來合併兩個 SegInfo，缺點是會有重新創建 SegInfo 物件的效能損失
+- 基本上只需要 SegInfo 的合併方法，以及 apply 方法即可，若遇到效能問題，可以考慮把需要重新創建 SegInfo 物件的部分改為直接賦值
 
-    Problem:
-    - 731. My Calendar II
+Problem:
+- 731. My Calendar II
+- P3372 【模板】线段树 1
 """
+
+class SegInfo:
+    def __init__(self, s: int = 0) -> None:
+        self.s = s
+
+    # 使用 + 運算符來合併兩個 SegInfo，但要注意效能問題
+    def __add__(self, other: 'SegInfo') -> 'SegInfo':
+        return SegInfo(self.s + other.s)
+
+    def __iadd__(self, other: 'SegInfo') -> 'SegInfo':  # self += other
+        self.s += other.s
+        return self
+
 
 class SegNode:
     def __init__(self) -> None:
-        self.ls = self.rs = None # left and right child
-        self.val = self.lazy = 0 # value, lazy tag
+        self.ls = self.rs = None  # left and right child
+        self.val = SegInfo()  # value
+        self.lazy = 0  # lazy tag
+
 
 class SegmentTree:
-    def __init__(self):
+    def __init__(self, nums: List[int] = None):
+        self.n = len(nums)
         self.root = SegNode()
-    
-    # update the range [l, r] with value v
-    @staticmethod
-    def update(node: SegNode, left: int, right: int, l: int, r: int, v: int) -> None:
-        if l <= left and right <= r:
-            node.lazy += v
-            node.val += v
+        if nums is not None and len(nums) > 0:
+            self.build(self.root, 1, self.n, nums)
+
+    def update(self, l: int, r: int, v: int) -> None:
+        self._update(self.root, 1, self.n, l, r, v)
+
+    def query(self, l: int, r: int) -> SegInfo:
+        return self._query(self.root, 1, self.n, l, r)
+
+    def build(self, node: SegNode, left: int, right: int, nums: List[int]) -> None:
+        if left == right:
+            node.val = SegInfo(nums[left - 1])
+            # 若使用 SegInfo(nums[left - 1]) 超時，考慮直接賦值
+            # node.val.s = nums[left - 1]
             return
-        SegmentTree.pushdown(node) # push down lazy tags
+        mid = (left + right) // 2
+        node.ls = SegNode()
+        node.rs = SegNode()
+        self.build(node.ls, left, mid, nums)
+        self.build(node.rs, mid + 1, right, nums)
+        self.pushup(node)  # push up node value
+
+    # update the range [l, r] with value v
+    def _update(self, node: SegNode, left: int, right: int, l: int, r: int, v: int) -> None:
+        if l <= left and right <= r:
+            # apply lazy tag
+            self.apply(node, left, right, v)
+            return
+        self.pushdown(node, left, right)  # push down lazy tags
         mid = (left + right) // 2
         if l <= mid:
-            SegmentTree.update(node.ls, left, mid, l, r, v)
+            self._update(node.ls, left, mid, l, r, v)
         if r > mid:
-            SegmentTree.update(node.rs, mid + 1, right, l, r, v)
-        SegmentTree.pushup(node) # push up node value
- 
+            self._update(node.rs, mid + 1, right, l, r, v)
+        self.pushup(node)  # push up node value
+
+    # apply lazy tag
+    def apply(self, node: SegNode, left: int, right: int, v: int) -> None:
+        node.val.s += v * (right - left + 1)
+        node.lazy += v
+
     # query the range [l, r]
-    @staticmethod
-    def query(node: SegNode, left: int, right: int, l: int, r: int) -> int:
+    def _query(self, node: SegNode, left: int, right: int, l: int, r: int) -> SegInfo:
         if l <= left and right <= r:
             return node.val
         # Ensure all lazy tags have been pushed down
-        SegmentTree.pushdown(node) 
-        # Calculate answer: maximum value in this problem
-        ans = 0
+        self.pushdown(node, left, right)
         mid = (left + right) // 2
+        # Calculate answer (Customized)
+        ans = SegInfo()
         if l <= mid:
-            ans = SegmentTree.query(node.ls, left, mid, l, r)
+            ans += self._query(node.ls, left, mid, l, r)
         if r > mid:
-            ans = max(ans, SegmentTree.query(node.rs, mid + 1, right, l, r))
+            ans += self._query(node.rs, mid + 1, right, l, r)
         return ans
-    
+
     # push down lazy tags
-    @staticmethod
-    def pushdown(node: SegNode) -> None:
+    def pushdown(self, node: SegNode, left: int, right: int) -> None:
         if node.ls is None:
             node.ls = SegNode()
         if node.rs is None:
             node.rs = SegNode()
-        if node.lazy:
-            node.ls.lazy += node.lazy
-            node.rs.lazy += node.lazy
-            node.ls.val += node.lazy
-            node.rs.val += node.lazy
-            node.lazy = 0
-    
+        if node.lazy != 0:
+            # apply lazy tag
+            mid = (left + right) // 2
+            self.apply(node.ls, left, mid, node.lazy)
+            self.apply(node.rs, mid + 1, right, node.lazy)
+            node.lazy = 0  # reset lazy tag
+
     # push up node value
-    @staticmethod
-    def pushup(node: SegNode) -> None:
-        # Update method: maximum value in this problem
-        node.val = max(node.ls.val, node.rs.val)
+    def pushup(self, node: SegNode) -> None:
+        # update node value
+        node.val = node.ls.val + node.rs.val
+        # 若使用 + 超時，考慮直接賦值
+        # node.val.s = node.ls.val.s + node.rs.val.s
