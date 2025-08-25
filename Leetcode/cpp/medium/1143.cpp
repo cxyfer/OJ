93b29a5d57f72d22a530290272e7f1833021b732
cxyfer <gdst.tw@gmail.com>
2025-08-26 01:23:46 +0800

    ✨ feat: Add diverse DP solutions for LCS
    Introduces three distinct dynamic programming approaches for the Longest Common Subsequence problem: memoization, tabulation, and push DP. This provides a comprehensive set of solutions demonstrating different DP paradigms.

--- Leetcode/cpp/medium/1143.cpp
+++ Leetcode/cpp/medium/1143.cpp
@@ -10,23 +10,59 @@
 using namespace std;
 // @lcpr-template-end
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
     int longestCommonSubsequence(string s, string t) {
         int m = s.size(), n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[m][n];
+        vector<vector<int>> memo(m, vector<int>(n, -1));
+        auto dfs = [&](this auto&& dfs, int i, int j) -> int {
+            if (i < 0 || j < 0) return 0;
+            int& res = memo[i][j];
+            if (res != -1) return res;
+            if (s[i] == t[j]) return res = dfs(i - 1, j - 1) + 1;
+            return res = max(dfs(i - 1, j), dfs(i, j - 1));
+        };
+        return dfs(m - 1, n - 1);
     }
 };
+
+class Solution2 {
+public:
+    int longestCommonSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        vector<vector<int>> f(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                if (s[i - 1] == t[j - 1])
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                else
+                    f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+        return f[m][n];
+    }
+};
+
+class Solution3 {
+public:
+    int longestCommonSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        s = " " + s + " ";
+        t = " " + t + " ";
+        vector<vector<int>> f(m + 2, vector<int>(n + 2));
+        auto checkmax = [&](int& a, int b) {
+            if (b > a) a = b;
+        };
+        for (int i = 0; i <= m; ++i)
+            for (int j = 0; j <= n; ++j) {
+                checkmax(f[i + 1][j], f[i][j]);
+                checkmax(f[i][j + 1], f[i][j]);
+                checkmax(f[i + 1][j + 1], f[i][j] + (s[i + 1] == t[j + 1]));
+            }
+        return f[m][n];
+    }
+};
+
+// using Solution = Solution1;
+// using Solution = Solution2;
+using Solution = Solution3;
 // @lc code=end
 
--- Leetcode/python3/medium/1143_Longest Common Subsequence.py
+++ Leetcode/python3/medium/1143_Longest Common Subsequence.py
@@ -9,23 +9,59 @@
 # @lcpr-template-start
 from preImport import *
 # @lcpr-template-end
+"""
+1. Memoization
+2. 查表法
+3. 刷表法
+"""
 # @lc code=start
-class Solution:
+max = lambda x, y: x if x > y else y
+class Solution1:
     def longestCommonSubsequence(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        @cache
+        def f(i: int, j: int) -> int:
+            if i < 0 or j < 0:
+                return 0
+            if s[i] == t[j]:
+                return f(i - 1, j - 1) + 1
+            return max(f(i - 1, j), f(i, j - 1))
+        return f(m - 1, n - 1)
 
+class Solution2:
+    def longestCommonSubsequence(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        # f[i][j] 表示 s[:i] 和 t[:j] 的 LCS 長度
+        f = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                if s[i - 1] == t[j - 1]: # 可由左上方轉移
-                    dp[i][j] = dp[i - 1][j - 1] + 1 
-                else: # 可由上方或左方轉移
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        return dp[m][n]
+                if s[i - 1] == t[j - 1]:  # 可由左上方轉移
+                    f[i][j] = f[i - 1][j - 1] + 1 
+                else:  # 可由上方或左方轉移
+                    f[i][j] = max(f[i - 1][j], f[i][j - 1])
+        return f[m][n]
+
+class Solution3:
+    def longestCommonSubsequence(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        s = " " + s + " "
+        t = " " + t + " "
+        # f[i][j] 表示 s[:i] 和 t[:j] 的 LCS 長度
+        f = [[0] * (n + 2) for _ in range(m + 2)]
+        for i in range(m + 1):
+            for j in range(n + 1):
+                f[i + 1][j] = max(f[i + 1][j], f[i][j])
+                f[i][j + 1] = max(f[i][j + 1], f[i][j])
+                f[i + 1][j + 1] = max(f[i + 1][j + 1], f[i][j] + (s[i + 1] == t[j + 1]))
+        return f[m][n]
+
+# Solution = Solution1
+Solution = Solution2
+# Solution = Solution3
 # @lc code=end
 
 sol = Solution()
-print(sol.longestCommonSubsequence("CGTTCGGAATG", "TCGAGTGC"))
+print(sol.longestCommonSubsequence("CGTTCGGAATG", "TCGAGTGC"))  # 6
 
 #
 # @lcpr case=start
