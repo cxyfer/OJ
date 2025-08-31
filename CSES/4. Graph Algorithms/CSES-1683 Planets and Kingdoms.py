82fbeeba7bfbc5dcfd558e32bd5224f4fa87164c
cxyfer <gdst.tw@gmail.com>
2025-08-31 10:25:17 +0800

    ✨ feat: Add CSES 1683 Planets and Kingdoms solution
    Implements Tarjan's algorithm to find Strongly Connected Components (SCCs).
    Utilizes an iterative DFS approach for efficient SCC identification and assignment of component IDs.

---
+++ CSES/4. Graph Algorithms/CSES-1683 Planets and Kingdoms.py
@@ -0,0 +1,90 @@
+class SCC:
+    def __init__(self, n: int):
+        self.n = n
+        self.g = [[] for _ in range(n)]
+
+        # --- Tarjan & SCC 相關 ---
+        self.time = 0
+        self.dfn = [-1] * n
+        self.low = [-1] * n
+        self.stk = []
+        self.in_stk = [False] * n
+        self.scc_id = [-1] * n
+        self.scc_cnt = 0
+
+    def add_edge(self, u: int, v: int):
+        self.g[u].append(v)
+
+    def dfs(self, u: int):
+        """遞迴版本"""
+        # self.dfn[u] = self.low[u] = self.time
+        # self.time += 1
+        # self.stk.append(u)
+        # self.in_stk[u] = True
+        # for v in self.g[u]:
+        #     if self.dfn[v] == -1:
+        #         self.dfs(v)
+        #         self.low[u] = min(self.low[u], self.low[v])
+        #     elif self.in_stk[v]:
+        #         self.low[u] = min(self.low[u], self.dfn[v])
+        # if self.dfn[u] == self.low[u]:
+        #     while True:
+        #         v = self.stk.pop()
+        #         self.in_stk[v] = False
+        #         self.scc_id[v] = self.scc_cnt
+        #         if v == u:
+        #             break
+        #     self.scc_cnt += 1
+        """迭代版本"""
+        st = [(u, 0)]  # (u, i)
+        while st:
+            u, i = st.pop()
+            if i == 0:
+                self.dfn[u] = self.low[u] = self.time
+                self.time += 1
+                self.stk.append(u)
+                self.in_stk[u] = True
+            if i > 0:  # 處理 i - 1 的返回邏輯
+                v = self.g[u][i-1]
+                self.low[u] = min(self.low[u], self.low[v])
+            for j in range(i, len(self.g[u])):
+                v = self.g[u][j]
+                if self.dfn[v] == -1:
+                    st.append((u, j + 1))
+                    st.append((v, 0))
+                    break
+                elif self.in_stk[v]:
+                    self.low[u] = min(self.low[u], self.dfn[v])
+            else:
+                if self.dfn[u] == self.low[u]:
+                    while True:
+                        v = self.stk.pop()
+                        self.in_stk[v] = False
+                        self.scc_id[v] = self.scc_cnt
+                        if v == u:
+                            break
+                    self.scc_cnt += 1
+
+    def sccs(self):
+        sccs = [[] for _ in range(self.scc_cnt)]
+        for u in range(self.n):
+            sccs[self.scc_id[u]].append(u)
+        return sccs
+
+    def run(self):
+        for u in range(self.n):
+            if self.dfn[u] == -1:
+                self.dfs(u)
+
+def solve():
+    n, m = map(int, input().split())
+    T = SCC(n)
+    for _ in range(m):
+        u, v = map(int, input().split())
+        T.add_edge(u - 1, v - 1)
+    T.run()
+    print(T.scc_cnt)
+    print(*[x + 1 for x in T.scc_id])
+
+if __name__ == "__main__":
+    solve()
