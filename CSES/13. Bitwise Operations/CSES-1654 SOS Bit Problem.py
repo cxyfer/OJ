d01aea85afef0be46b6677c646d4fbe10e144157
cxyfer <gdst.tw@gmail.com>
2025-09-01 00:08:28 +0800

    🐛 fix: Correct SOS DP iteration for sum over supersets
    Updates the `f2` calculation to use the standard SOS DP approach, ensuring correct results for sum over supersets. Adds explanatory comments detailing common bitwise operations and their SOS DP interpretations.

--- CSES/13. Bitwise Operations/CSES-1654 SOS Bit Problem.py
+++ CSES/13. Bitwise Operations/CSES-1654 SOS Bit Problem.py
@@ -1,8 +1,13 @@
 """
 CSES-1654 SOS Bit Problem
 https://cses.fi/problemset/task/1654
-
 SOS DP
+
+1. 滿足 (x | y) == x 的 y 個數：x 的子集中的元素數量
+2. 滿足 (x & y) == x 的 y 個數：x 的超集中的元素數量
+3. 滿足 (x & y) != 0 的 y 個數：
+  - 正難則反，所求為全部元素數量 n 減去 (x & y) == 0 的 y 個數
+  - 而滿足 (x & y) == 0 的 y 個數，等價於 (~x | y) == (~x) 的 y 個數，故由 1. 可得
 """
 def solve():
     n = int(input())
@@ -28,19 +33,13 @@ def solve():
             s += 1
 
     for i in range(BITS):
-        # for s in range(U - 1, -1, -1):
-        #     if (s >> i) & 1:
-        #         f2[s ^ (1 << i)] += f2[s]
-        s = 0
-        while s < U:
-            s |= (1 << i)
-            if s < U:
+        # 這裡是從大的子集往小的子集更新，反過來做似乎也是對的
+        for s in range(U - 1, -1, -1):
+            if (s >> i) & 1:
                 f2[s ^ (1 << i)] += f2[s]
-            s += 1
 
     for x in A:
         print(f1[x], f2[x], n - f1[x ^ ((1 << BITS) - 1)])
 
 if __name__ == '__main__':
     solve()
-
