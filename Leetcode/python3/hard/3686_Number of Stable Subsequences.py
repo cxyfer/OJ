dcf14bc1e798f09af26b90daa750256c83e3b9de
cxyfer <gdst.tw@gmail.com>
2025-09-15 01:30:59 +0800

    ✨ feat: Add alternative DP solution for stable subsequences
    Introduces a new dynamic programming approach to solve the problem.
    This alternative solution uses a refined DP state to count stable subsequences, offering a different strategy for problem resolution.

--- Leetcode/python3/hard/3686_Number of Stable Subsequences.py
+++ Leetcode/python3/hard/3686_Number of Stable Subsequences.py
@@ -10,7 +10,7 @@ from preImport import *
 # @lcpr-template-end
 # @lc code=start
 MOD = int(1e9 + 7)
-class Solution:
+class Solution1:
     def countStableSubsequences(self, nums: List[int]) -> int:
         # f[i][b][j] 表示考慮前 i 個元素，最後為連續 j 個 b 的子序列數量
         # f = [[[0, 0] for _ in range(2)] for _ in range(n + 1)]
@@ -20,6 +20,37 @@ class Solution:
             f[b][1] = (f[b][1] + f[b][0]) % MOD
             f[b][0] = (f[b][0] + f[b ^ 1][0] + f[b ^ 1][1] + 1) % MOD
         return sum(map(sum, f)) % MOD
+
+class Solution2:
+    def countStableSubsequences(self, nums: List[int]) -> int:
+        # f[i][b1][b2] 表示考慮前 i 個元素，最後兩個元素分別為 b1 和 b2 ，且不出現超過連續三個相同元素的子序列數量
+        f = [[0, 0, 0] for _ in range(3)]
+        for x in nums:
+            b = x & 1
+            f[b][b] += (f[b ^ 1][b] + f[2][b])
+            f[b][b] %= MOD
+            f[b ^ 1][b] += (f[0][b ^ 1] + f[1][b ^ 1] + f[2][b ^ 1])
+            f[b ^ 1][b] %= MOD
+            f[2][b] += 1
+            f[2][b] %= MOD
+            # if b == 0:  # 拆開來會比較好懂
+            #     f[0][0] += (f[1][0] + f[2][0])
+            #     f[0][0] %= MOD
+            #     f[1][0] += (f[0][1] + f[1][1] + f[2][1])
+            #     f[1][0] %= MOD
+            #     f[2][0] += 1
+            #     f[2][0] %= MOD
+            # else:
+            #     f[1][1] += (f[0][1] + f[2][1])
+            #     f[1][1] %= MOD
+            #     f[0][1] += (f[0][0] + f[1][0] + f[2][0])
+            #     f[0][1] %= MOD
+            #     f[2][1] += 1
+            #     f[2][1] %= MOD
+        return sum(map(sum, f)) % MOD
+
+# Solution = Solution1
+Solution = Solution2
 # @lc code=end
 sol = Solution()
 print(sol.countStableSubsequences([1,3,5]))  # 6
