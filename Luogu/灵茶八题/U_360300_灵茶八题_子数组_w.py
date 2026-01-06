0c4347afdd55ab9f974952d311e00fe024f53ec4
cxyfer <gdst.tw@gmail.com>
2026-01-06 19:57:39 +0800

    📝docs: Add problem descriptions and links
    Adds problem descriptions and links to the Luogu contest problems.
    
    This enhances code readability and provides context for future reference.

--- Luogu/灵茶八题/U_360300_灵茶八题_子数组_w.py
+++ Luogu/灵茶八题/U_360300_灵茶八题_子数组_w.py
@@ -1,7 +1,10 @@
 """
-    貢獻法 + 乘法原理
-    包含 A[i] 的子陣列左端點有 i+1 個，右端點有 n-i 個
-    所以包含 A[i] 的子陣列有 (i+1)*(n-i) 個
+U360300 灵茶八题 - 子数组 +w+
+https://www.luogu.com.cn/problem/U360300
+
+貢獻法 + 乘法原理
+包含 A[i] 的子陣列左端點有 i+1 個，右端點有 n-i 個
+所以包含 A[i] 的子陣列有 (i+1)*(n-i) 個
 """
 n = int(input())
 A = list(map(int, input().split()))
--- Luogu/灵茶八题/U_360640_灵茶八题_子序列_w.py
+++ Luogu/灵茶八题/U_360640_灵茶八题_子序列_w.py
@@ -1,9 +1,12 @@
 """
-    包含 A[i] 的子序列數量 = 2^(n-1)
+U360640 灵茶八题 - 子序列 +w+
+https://www.luogu.com.cn/problem/U360640
+
+考慮貢獻，包含 A[i] 的子序列數量有 2^(n-1) 個
 """
 n = int(input())
 A = list(map(int, input().split()))
-MOD = 10**9 + 7
+MOD = int(1e9 + 7)
 
 p = pow(2, n - 1, MOD)
 
