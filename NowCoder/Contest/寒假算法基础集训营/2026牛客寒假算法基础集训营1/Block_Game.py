b5d9ec2384b5fa201a1c9f9b744b267da3f5a594
cxyfer <gdst.tw@gmail.com>
2026-02-04 17:29:06 +0800

    ✨ feat(nowcoder): add solution for Block Game
    Adds a Python solution for the Block Game problem from the 2026 NowCoder Winter Camp.
    
    Implements an efficient approach to find the maximum sum of adjacent elements in a circular array formed by the input sequence and an additional constant.

---
+++ NowCoder/Contest/寒假算法基础集训营/2026牛客寒假算法基础集训营1/Block_Game.py
@@ -0,0 +1,20 @@
+"""
+E. Block Game
+https://ac.nowcoder.com/acm/contest/120561/E
+其實就是在 A + [k] 上循環操作，每次將最後面的數字移到最前面，所求為隊首和隊尾的數字和最大值
+由於在循環意義下，隊首和隊尾是相鄰的，因此所求是 A + [k] 中循環相鄰元素的最大值
+"""
+def solve():
+    n, k = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    A.append(k)
+    ans = A[0] + k
+    for a, b in zip(A, A[1:]):
+        ans = max(ans, a + b)
+    print(ans)
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        solve()
