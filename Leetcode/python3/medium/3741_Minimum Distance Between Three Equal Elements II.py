00921519691cc66ed15d7e5d7d983ba609a11809
cxyfer <gdst.tw@gmail.com>
2026-04-10 14:11:18 +0800

    ✨ feat(leetcode): add solutions for problems 3740 and 3741
    Implement the minimum distance calculation logic for finding three equal elements in an array.

---
+++ Leetcode/python3/medium/3741_Minimum Distance Between Three Equal Elements II.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=3741 lang=python3
+#
+# [3741] Minimum Distance Between Three Equal Elements II
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = float('inf')
+        pos = [[] for _ in range(n + 1)]
+        for i, x in enumerate(nums):
+            if len(pos[x]) >= 2:
+                ans = min(ans, (i - pos[x][-2]) << 1)
+            pos[x].append(i)
+        return ans if ans < float('inf') else -1
+# @lc code=end
+
