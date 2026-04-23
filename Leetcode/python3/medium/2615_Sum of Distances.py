ac72b50b73db4fb5559126be73b69f6e0b7f072b
cxyfer <gdst.tw@gmail.com>
2026-04-23 17:47:38 +0800

    ✨ feat: add sum of distances solution
    Computes pairwise index distances by grouping equal values and using prefix sums to get each position’s total distance in linear time per group.

---
+++ Leetcode/python3/medium/2615_Sum of Distances.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=2615 lang=python3
+#
+# [2615] Sum of Distances
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pos = defaultdict(list)
+        for i, x in enumerate(nums):
+            pos[x].append(i)
+        
+        ans = [0] * n
+        for x, idxs in pos.items():
+            m = len(idxs)
+            s = list(accumulate(idxs, initial=0))
+            for i, idx in enumerate(idxs, start=1):
+                L = idx * (i - 1) - s[i - 1]
+                R = (s[m] - s[i]) - idx * (m - i)
+                ans[idx] = L + R
+        return ans
+# @lc code=end
+
