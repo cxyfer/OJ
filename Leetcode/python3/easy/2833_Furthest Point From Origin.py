51d4db5a2e89f293bc33945456ad6b59d89ed25f
cxyfer <gdst.tw@gmail.com>
2026-04-24 09:35:13 +0800

    ✨ feat(leetcode): add solution for furthest point
    Adds a greedy distance calculation that maximizes final displacement by treating unknown moves as steps in the direction that increases absolute distance.

---
+++ Leetcode/python3/easy/2833_Furthest Point From Origin.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=2833 lang=python3
+#
+# [2833] Furthest Point From Origin
+#
+
+# @lc code=start
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        cnt = diff = 0
+        for ch in moves:
+            if ch =='L':
+                diff -= 1
+            elif ch == 'R':
+                diff += 1
+            else:
+                cnt += 1
+        return abs(diff) + cnt
+# @lc code=end
+
