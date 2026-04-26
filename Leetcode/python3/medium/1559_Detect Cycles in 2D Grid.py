af2f2e57c92f9c51d60573ab0fc50b90df35769f
cxyfer <gdst.tw@gmail.com>
2026-04-26 19:24:45 +0800

    ✨ feat: add cycle detection in 2d grid
    Implements a union-find based solution to detect same-letter cycles while scanning the grid.
    
    Adds quick validation on repeated connections to return as soon as a valid cycle is found.

---
+++ Leetcode/python3/medium/1559_Detect Cycles in 2D Grid.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode id=1559 lang=python3
+#
+# [1559] Detect Cycles in 2D Grid
+#
+
+
+# @lcpr-template-start
+from preImport import *
+# @lcpr-template-end
+# @lc code=start
+class UnionFind:
+    __slots__ = ['n', 'pa', 'sz', 'cnt']
+
+    def __init__(self, n: int):
+        self.n = n
+        self.pa = list(range(n))  # 父節點
+        self.sz = [1] * n  # 連通分量大小
+        self.cnt = n  # 連通分量數量
+
+    def find(self, x: int) -> int:
+        while self.pa[x] != x:
+            self.pa[x] = self.pa[self.pa[x]]
+            x = self.pa[x]
+        return x
+
+    def union(self, x: int, y: int) -> bool:
+        fx, fy = self.find(x), self.find(y)
+        if fx == fy:
+            return False
+        if self.sz[fx] < self.sz[fy]:
+            fx, fy = fy, fx
+        self.pa[fy] = fx
+        self.sz[fx] += self.sz[fy]
+        self.cnt -= 1
+        return True
+
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        n, m = len(grid), len(grid[0])
+        uf = UnionFind(n * m)
+
+        for i, row in enumerate(grid):
+            for j, x in enumerate(row):
+                if i > 0 and x == grid[i - 1][j]:
+                    if not uf.union(i * m + j, (i - 1) * m + j) and uf.sz[uf.find(i * m + j)] >= 4:
+                        return True
+                if j > 0 and x == grid[i][j - 1]:
+                    if not uf.union(i * m + j, i * m + j - 1) and uf.sz[uf.find(i * m + j)] >= 4:
+                        return True
+        return False
+# @lc code=end
+
+sol = Solution()
+grid = [["a","a","a","a"],["a","b","b","a"],["a","b","b","a"],["a","a","a","a"]]
+print(sol.containsCycle(grid))  # true
+grid = [["c","c","c","a"],["c","d","c","c"],["c","c","e","c"],["f","c","c","c"]]
+print(sol.containsCycle(grid))  # true
+grid = [["a","b","b"],["b","z","b"],["b","b","a"]]
+print(sol.containsCycle(grid))  # false
+grid = [["b","a","c"],["c","a","c"],["d","d","c"],["b","c","c"]]
+print(sol.containsCycle(grid))  # false
