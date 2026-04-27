029766c3a2a3e78e9b709b5eb3a62466b72b9dd9
cxyfer <gdst.tw@gmail.com>
2026-04-27 21:16:13 +0800

    📝 docs(dsu): document path compression variants
    Adds inline notes showing recursive, path-halving, and full path-compression approaches for find, making the implementation choices and tradeoffs easier to understand.

--- Template/DataStructure/DSU.py
+++ Template/DataStructure/DSU.py
@@ -8,10 +8,25 @@ class UnionFind:
         self.cnt = n  # 連通分量數量
 
     def find(self, x: int) -> int:
+        """Recursive version, full path compression"""
+        # if self.pa[x] != x:
+        #     self.pa[x] = self.find(self.pa[x])
+        # return self.pa[x]
+        """Iterative version, path halving"""
         while self.pa[x] != x:
             self.pa[x] = self.pa[self.pa[x]]
             x = self.pa[x]
         return x
+        """Iterative version, full path compression"""
+        # path = []
+        # curr = x
+        # while self.pa[curr] != curr:
+        #     path.append(curr)
+        #     curr = self.pa[curr]
+        # root = curr
+        # for node in reversed(path):
+        #     self.pa[node] = root
+        # return root
 
     def union(self, x: int, y: int) -> bool:
         fx, fy = self.find(x), self.find(y)
