a94e63bdd56e4cba9c0c459be841348291cd78ed
cxyfer <gdst.tw@gmail.com>
2026-05-13 01:06:54 +0800

    ✨ feat(AtCoder): refactor C_Snake_Numbers to use a single solve function with improved DFS logic

--- AtCoder/ABC/ABC387/C_Snake_Numbers.py
+++ AtCoder/ABC/ABC387/C_Snake_Numbers.py
@@ -1,33 +1,35 @@
 from functools import cache
 
-def count(num: int) -> int:
-    s = str(num)
-    n = len(s)
+
+def solve():
+    l, r = map(int, input().split())
+
+    high = list(map(int, str(r)))
+    n = len(high)
+    low = list(map(int, str(l).zfill(n)))  # 補前導零，使 low 和 high 對齊
 
     @cache
-    def dfs(i: int, is_limit: bool, first_digit: int) -> int:
-        """
-        數位DP
-        
-        i: 當前位數
-        is_limit: 當前位是否受到上界限制
-        first_digit: 首位數字，相當於模板的 is_num
-        """
+    def dfs(i: int, top: int, limit_low: bool, limit_high: bool) -> int:
         if i == n:
-            return int(first_digit > 0)
-        
-        upper = int(s[i]) if is_limit else 9
-        res = 0
-        if not first_digit:
-            res += dfs(i + 1, False, 0)
-            for d in range(1, upper + 1):
-                res += dfs(i + 1, is_limit and d == upper, d)
-        else:
-            for d in range(0, min(first_digit - 1, upper) + 1):
-                res += dfs(i + 1, is_limit and d == upper, first_digit)
-        return res
-    
-    return dfs(0, True, 0)
+            return 1 if top > 0 else 0
 
-L, R = map(int, input().split())
-print(count(R) - count(L - 1))
+        # 第 i 個數位可以從 lo 枚舉到 hi
+        # 如果對數位還有其它約束，應該只在下面的 for 迴圈做限制，不應修改 lo 或 hi
+        lo = low[i] if limit_low else 0
+        hi = high[i] if limit_high else 9
+
+        res = 0
+        for d in range(lo, hi + 1):
+            if top == 0:
+                res += dfs(i + 1, d, limit_low and d == lo, limit_high and d == hi)
+            elif d < top:
+                res += dfs(i + 1, top, limit_low and d == lo, limit_high and d == hi)
+        return res
+
+    ans = dfs(0, 0, True, True)
+    dfs.cache_clear()
+    print(ans)
+
+
+if __name__ == "__main__":
+    solve()
