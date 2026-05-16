0b1b617381c74dd3167f783b1849beba19d1790c
cxyfer <gdst.tw@gmail.com>
2026-05-16 11:54:10 +0800

    ♻️ refactor(easy): add alternate distance value solutions
    Introduces two implementations for the distance-value check and switches the active one to a more efficient sorted two-pointer approach.
    
    Also tightens the range test to avoid edge-case misses when comparing values within the allowed distance.

--- Leetcode/python3/easy/1385_Find the Distance Value Between Two Arrays.py
+++ Leetcode/python3/easy/1385_Find the Distance Value Between Two Arrays.py
@@ -7,16 +7,35 @@
 
 # @lcpr-template-start
 from preImport import *
+
+
 # @lcpr-template-end
 # @lc code=start
-class Solution:
+class Solution1:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
         arr2.sort()
         ans = 0
         for x in arr1:
-            idx = bisect_left(arr2, x - d)
-            if idx == len(arr2) or arr2[idx] > x + d:
+            idx1 = bisect_left(arr2, x - d)
+            idx2 = bisect_right(arr2, x + d) - 1
+            if idx1 > idx2:
                 ans += 1
         return ans
-# @lc code=end
 
+
+class Solution2:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        arr1.sort()
+        arr2.sort()
+        ans = i = j = 0
+        for x in arr1:
+            while i < len(arr2) and arr2[i] < x - d:
+                i += 1
+            if i == len(arr2) or arr2[i] > x + d:
+                ans += 1
+        return ans
+
+
+# Solution = Solution1
+Solution = Solution2
+# @lc code=end
