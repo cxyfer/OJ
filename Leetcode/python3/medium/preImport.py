e36a6171ead8e4a6f360aa403b2e0962112f8915
cxyfer <gdst.tw@gmail.com>
2026-05-18 21:39:57 +0800

    🔧 chore: add shared math import and normalize formatting
    Adds a common infinity import for upcoming solutions and tidies the shared pre-import helpers.
    
    Also normalizes spacing and file endings across the problem templates to keep the Python setup consistent.

--- Leetcode/python3/medium/preImport.py
+++ Leetcode/python3/medium/preImport.py
@@ -6,18 +6,23 @@ from collections import *
 from functools import *
 from operator import *
 from string import *
+
 del pow
 
+from math import inf
 from heapq import *
 from bisect import *
 from itertools import *
 from random import *
 from sortedcontainers import *
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
