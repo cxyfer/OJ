be4cb59ede5f6272a0f2ca10954e847fb3a65fae
cxyfer <gdst.tw@gmail.com>
2026-06-23 22:38:39 +0800

    ♻️ refactor(leetcode): 295 - simplify median heap handling
    Uses native max-heap operations for the lower half instead of storing negated values.
    
    Improves readability and makes the median balancing logic more direct and less error-prone.

--- Leetcode/python3/hard/295_Find Median from Data Stream.py
+++ Leetcode/python3/hard/295_Find Median from Data Stream.py
@@ -11,31 +11,31 @@ from preImport import *
 # @lcpr-template-end
 # @lc code=start
 class MedianFinder:
-
     def __init__(self):
         # max heap，保存 <= median 的數字
-        self.l = []
+        self.L = []
         # min heap，保存 > median 的數字
-        self.r = []
+        self.R = []
 
     def addNum(self, num: int) -> None:
         # 當前數量為偶數，加入到左邊
-        if len(self.l) == len(self.r):
+        if len(self.L) == len(self.R):
             # 如果新加入的數字比右邊的最小值還要大，則先加入到右邊，再將右邊的最小值加入到左邊
-            if self.r and num > self.r[0]:
-                heappush(self.l, -heappushpop(self.r, num))
+            if self.R and num > self.R[0]:
+                heappush_max(self.L, heappushpop(self.R, num))
             else:
-                heappush(self.l, -num)
+                heappush_max(self.L, num)
         # 當前數量為奇數，加入到右邊
         else:
             # 如果新加入的數字比左邊的最大值還要小，則先加入到左邊，再將左邊的最大值加入到右邊
-            if num < -self.l[0]:
-                heappush(self.r, -heappushpop(self.l, -num))
+            if num < self.L[0]:
+                heappush(self.R, heappushpop_max(self.L, num))
             else:
-                heappush(self.r, num)
+                heappush(self.R, num)
 
     def findMedian(self) -> float:
-        return (-self.l[0] + self.r[0]) / 2 if len(self.l) == len(self.r) else -self.l[0]
+        return (self.L[0] + self.R[0]) / 2 if len(self.L) == len(self.R) else self.L[0]
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
