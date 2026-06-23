19a3f83f0ded1f86e549c305ddab306f05a66662
cxyfer <gdst.tw@gmail.com>
2026-06-23 23:26:35 +0800

    ✨ feat(leetcode): 312. Burst Balloons - update active DP solution
    Improves the dynamic programming implementation and makes it the default approach.
    
    Clarifies the interval-state transitions, simplifies the recurrence updates, and cleans up formatting for easier comparison between the available solutions.

--- Leetcode/python3/hard/312_Burst Balloons.py
+++ Leetcode/python3/hard/312_Burst Balloons.py
@@ -25,27 +25,31 @@ class Solution1:
                 return 0
             res = 0
             for i in range(left + 1, right):
-                cur = nums[left] * nums[i] * nums[right]
-                cur += dfs(left, i) + dfs(i, right)
-                res = max(res, cur)
+                res = max(
+                    res,
+                    dfs(left, i) + dfs(i, right) + nums[left] * nums[i] * nums[right],
+                )
             return res
 
         return dfs(0, n + 1)
-    
+
+
 class Solution2:
     def maxCoins(self, nums: List[int]) -> int:
         n = len(nums)
         nums = [1] + nums + [1]
-        dp = [[0] * (n + 2) for _ in range(n + 2)]
-        for ln in range(3, n + 3):  # length of subarray
-            for l in range(n + 3 - ln):
-                r = l + ln - 1
+        f = [[0] * (n + 2) for _ in range(n + 2)]
+        for ln in range(1, n + 1):  # number of balloons inside (l, r)
+            for l in range(n - ln + 1):
+                r = l + ln + 1
                 for m in range(l + 1, r):
-                    dp[l][r] = max(
-                        dp[l][r],
-                        dp[l][m] + dp[m][r] + nums[l] * nums[m] * nums[r])
-        return dp[0][n + 1]
-    
+                    f[l][r] = max(
+                        f[l][r],
+                        f[l][m] + f[m][r] + nums[l] * nums[m] * nums[r],
+                    )
+        return f[0][n + 1]
+
+
 class Solution3:
     def maxCoins(self, nums: List[int]) -> int:
         p = [1] + nums + [1]
@@ -58,18 +62,19 @@ class Solution3:
         for i in range(n + 1):
             dp[i][i] = 0
 
-        for ln in range(2, n + 1): # 枚舉長度
-            for i in range(1, n + 1 - (ln - 1)): # 枚舉起點
-                j = i + ln - 1 # 終點
+        for ln in range(2, n + 1):  # 枚舉長度
+            for i in range(1, n + 1 - (ln - 1)):  # 枚舉起點
+                j = i + ln - 1  # 終點
                 for k in range(i, j):
-                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j])
-                    
+                    dp[i][j] = max(
+                        dp[i][j], dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j]
+                    )
+
         return dp[1][n]
 
-# class Solution(Solution1):
-# class Solution(Solution2):  
-class Solution(Solution3):
-    pass
+
+# Solution = Solution1
+Solution = Solution2
 # @lc code=end
 
 sol = Solution()
