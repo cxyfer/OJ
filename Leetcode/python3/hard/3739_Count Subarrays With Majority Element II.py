63dde4198639a697919934a5b44605b6da12ae64
cxyfer <gdst.tw@gmail.com>
2026-06-26 15:42:04 +0800

    ✨ feat(leetcode): 3739. Count Subarrays With Majority Element II - add divide and conquer solution
    Improves the solution set with an alternative approach that counts valid subarrays more efficiently.
    
    Updates the default implementation to use the new method for better performance on larger inputs.

--- Leetcode/python3/hard/3739_Count Subarrays With Majority Element II.py
+++ Leetcode/python3/hard/3739_Count Subarrays With Majority Element II.py
@@ -6,6 +6,8 @@
 
 
 # @lcpr-template-start
+from re import S
+
 from preImport import *
 
 # @lcpr-template-end
@@ -98,9 +100,43 @@ class Solution3:
         return ans
 
 
+class Solution4:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        A = list(accumulate((1 if x == target else -1 for x in nums), initial=0))
+
+        def cdq(left, right):
+            if left >= right:
+                return 0
+            mid = (left + right) // 2
+            res = cdq(left, mid) + cdq(mid + 1, right)
+            i, j = left, mid + 1
+            tmp = []
+            while i <= mid and j <= right:
+                if A[i] >= A[j]:
+                    tmp.append(A[i])
+                    i += 1
+                else:
+                    tmp.append(A[j])
+                    j += 1
+                    res += mid - i + 1
+            while i <= mid:
+                tmp.append(A[i])
+                i += 1
+            while j <= right:
+                tmp.append(A[j])
+                j += 1
+            A[left : right + 1] = tmp
+            return res
+
+        return cdq(0, n)
+
+
 # Solution = Solution1
 # Solution = Solution2
-Solution = Solution3
+# Solution = Solution3
+Solution = Solution4
 # @lc code=end
 sol = Solution()
 print(sol.countMajoritySubarrays([1,2,2,3], 2))  # 5
