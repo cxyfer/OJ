c8663b18f47dbe0df5a59afee7713363b035a9db
cxyfer <gdst.tw@gmail.com>
2026-06-30 00:45:44 +0800

    ♻️ refactor(leetcode): 1358 - simplify sliding window state
    Replaces the tracked satisfied-count with a remaining-needed counter to make the window logic easier to follow.
    
    Also swaps the Python frequency map for a fixed-size array, which better matches the constrained character set and keeps both implementations consistent.

--- Leetcode/cpp/medium/1358.cpp
+++ Leetcode/cpp/medium/1358.cpp
@@ -14,17 +14,17 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size();
-        int ans = 0, left = 0, have = 0;
+        int ans = 0, left = 0, need = 3;
         vector<int> cnt(3);
         for (char& ch : s) {
             // 1. 入窗口
-            if (++cnt[ch - 'a'] == 1) have++;
+            if (++cnt[ch - 'a'] == 1) need--;
             // 2. 出窗口，維持窗口在向左延伸即可滿足條件的狀態
-            while (have == 3) {
-                if (--cnt[s[left] - 'a'] == 0) have--;
+            while (need == 0) {
+                if (--cnt[s[left] - 'a'] == 0) need++;
                 left++;
             }
-            // 3. 累加答案，即以 right 為右端點的子字串中，滿足條件的數量
+            // 3. 累加答案，此時 [0, left - 1] 都是合法的左端點
             ans += left;
         }
         return ans;
--- Leetcode/python3/medium/1358_Number of Substrings Containing All Three Characters.py
+++ Leetcode/python3/medium/1358_Number of Substrings Containing All Three Characters.py
@@ -12,20 +12,23 @@ from preImport import *
 # @lc code=start
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        ans = left = have = 0
-        cnt = defaultdict(int)
+        need = 3
+        ans = left = 0
+        cnt = [0] * 3
         for right, ch in enumerate(s):
+            c = ord(ch) - ord('a')
             # 1. 入窗口
-            cnt[ch] += 1
-            if cnt[ch] == 1:
-                have += 1
+            cnt[c] += 1
+            if cnt[c] == 1:
+                need -= 1
             # 2. 出窗口，維持窗口在向左延伸即可滿足條件的狀態
-            while (have == 3):
-                cnt[s[left]] -= 1
-                if cnt[s[left]] == 0:
-                    have -= 1
+            while (need == 0):
+                c = ord(s[left]) - ord('a')
+                cnt[c] -= 1
+                if cnt[c] == 0:
+                    need += 1
                 left += 1
-            # 3. 累加答案，即以 right 為右端點的子字串中，滿足條件的數量
+            # 3. 累加答案，此時 [0, left - 1] 都是合法的左端點
             ans += left
         return ans
 # @lc code=end
